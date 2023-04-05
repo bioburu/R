33e67ab6c8c4a352b461ba0aa7e9582bdbefe3a9
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2023-04-04 14:50:06 -1000

    Update Seurat_merging.R

--- Seurat_merging.R
+++ Seurat_merging.R
@@ -8,13 +8,17 @@ library(ggridges)
 setwd("/home/amp_prog/Downloads")
 features_path <- "GSE130116_features.tsv.gz"
 barcodes_path <- "GSE130116_barcodes.tsv.gz"
-matrix_path <- "GSM3732336_ETV001_NYU_DIAGNOSIS.matrix.mtx.gz"
+matrix_path <- "GSM3732339_ETV002_COG_RELAPSE.matrix.mtx.gz"
 m1 <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
-obj1<-CreateSeuratObject(counts = m1,project = 'Diagnosis')
-features_path <- "GSE130116_features.tsv.gz"
-barcodes_path <- "GSE130116_barcodes.tsv.gz"
+m1
+obj1<-CreateSeuratObject(counts = m1,project = 'Relapse')
 matrix_path <- "GSM3732337_ETV001_NYU_RELAPSE.matrix.mtx.gz"
 m2 <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+m2
 obj2<-CreateSeuratObject(counts = m2,project = 'Relapse')
-data <- merge(obj1, y = obj2, add.cell.ids = c("Diagnosis", "Relapse"), project = "rrBALL")
-break 
+obj1
+obj2
+data <- merge(obj1, y = obj2, add.cell.ids = c("Relapse", "Relapse"), project = "rrBALL")
+gc()
+head(data@meta.data)
+data@active.ident 
