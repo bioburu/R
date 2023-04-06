fc2c0c1e0a4f0a19c228a54e94fb224da0819702
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2023-04-05 16:19:38 -1000

    Update BiomaRt_template.R

--- BiomaRt_template.R
+++ BiomaRt_template.R
@@ -1,40 +1,38 @@
 library(biomaRt)
-#set your proxy
 Sys.setenv("http_proxy"="http://my.proxy.org:9999")
-#list available biomarts
 listMarts()
-#set mart object
 ensembl=useMart("ensembl")
-#list the different datasets available from the biomart database
 listDatasets(ensembl)
-#update the mart object by addition of dataset
 ensembl=useDataset("hsapiens_gene_ensembl",mart = ensembl)
-#add filter argument
 filters = listFilters(ensembl)
-filters[1:5,]
-#add attributes argument
 attributes=listAttributes(ensembl)
-attributes[1:5,]
-#load in whatever and clean
-setwd('/home/amp_prog/rstudio/CAR_T/GSE120649_kidney_rejection')
-matrix<-read.delim('GSM3406955_s1.txt.gz',header = FALSE)
+setwd("/home/amp_prog/Downloads")
+matrix <- read.delim('postBiTE.txt')
+matrix<-matrix[,-c(2:4)]
+colnames(matrix)<-c('Gene','postBiTE')
+#-----------------------------------------------------------------
 dim(matrix)
-geneid <-matrix$V1
+geneid <- matrix$Gene
 head(geneid)
-#align identifiers
+dim(geneid)
 listFilters(ensembl)
 listAttributes(ensembl)
-genes <-getBM(attributes = c('ensembl_gene_id_version','external_gene_name'),
-              filters = 'ensembl_gene_id_version',
-              values = geneid,
-              mart = ensembl)
+genes <-getBM(attributes = c('ensembl_transcript_id_version','external_gene_name'),
+      filters = 'ensembl_transcript_id_version',
+      values = geneid,
+      mart = ensembl)
 head(genes)
-df <- matrix[matrix$V1 %in% unique(genes$ensembl_gene_id_version),]
+df <- matrix[matrix$Gene %in% unique(genes$ensembl_transcript_id_version),]
 dim(df)
 dim(matrix)
-genes$ensembl_gene_id_version
-colnames(genes)[1]<- 'V1'
-matrix <- merge(genes, df, by="V1")
-matrix<- matrix[,-c(1)]
-colnames(matrix)<-c('Gene','AntibodyMrejection_1')
+View(df)
+genes$ensembl_transcript_id_version
+View(genes)
+colnames(genes)[1]<- 'Gene'
+Ftable <- merge(genes, df, by="Gene")
+View(Ftable)
+Ftable<- Ftable[,-c(1)]
+colnames(Ftable)[1] <- c('Gene')
+write.csv(Ftable,file = 'Relapse_1.csv')
+
 
