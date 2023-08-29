7871df4775f26a6be438031c6c427aae18d1a833
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2023-08-29 07:21:44 -1000

    Update bulk_list_searches.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- bulk_list_searches.R
+++ bulk_list_searches.R
@@ -6,3 +6,10 @@ list3<-read.csv('ESCC.csv')
 joined<-semi_join(list1, list2, by = c('x'))
 results<-semi_join(joined,list3,by=c('x'))
 write.csv(results,file = 'results.csv')
+
+###---to see significance of search results
+setwd('/home/amp_prog/Downloads')
+results<-read.csv('results.csv')
+list<-results$x
+x<-FindMarkers(data, ident.1 = 'ESCC', ident.2 = 'adjacent', features = c(list))
+write.csv(x,file = 'sig.ESCC.csv')
