fa1b5a3ebe2f278b27215a21921e242338460b46
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2023-08-31 05:38:54 -1000

    Update bulk_list_searches.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- bulk_list_searches.R
+++ bulk_list_searches.R
@@ -1,15 +1,15 @@
 library(dplyr)
 setwd('/home/amp_prog/Downloads')
-list1<-read.csv('cervical.csv')
-list2<-read.csv('CRC.csv')
-list3<-read.csv('ESCC.csv')
+list1<-read.csv('milr1.cerv.1000.csv')
+list2<-read.csv('milr1.ESCC.1000.csv')
+list3<-read.csv('milr1.CRC.1000.csv')
 joined<-semi_join(list1, list2, by = c('x'))
-results<-semi_join(joined,list3,by=c('x'))
+results<-semi_join(joined, list3, by = c('x'))
 write.csv(results,file = 'results.csv')
 
 ###---to see significance of search results
 setwd('/home/amp_prog/Downloads')
-results<-read.csv('results.csv')
-list<-results$x
-x<-FindMarkers(data, ident.1 = 'ESCC', ident.2 = 'adjacent', features = c(list))
-write.csv(x,file = 'sig.ESCC.csv')
+results<-read.csv('RESULTS.csv')
+list<-results$X
+setwd('/home/amp_prog/Downloads')
+write.csv(x,file = 'milr1.CRC.1000.csv')
