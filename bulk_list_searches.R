70c2a407cbbf7d75a791d3dbd719755bbaeae62e
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2023-09-15 11:21:18 -1000

    Update bulk_list_searches.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- bulk_list_searches.R
+++ bulk_list_searches.R
@@ -1,15 +1,14 @@
 library(dplyr)
-setwd('/home/amp_prog/Downloads')
-list1<-read.csv('milr1.cerv.1000.csv')
-list2<-read.csv('milr1.ESCC.1000.csv')
-list3<-read.csv('milr1.CRC.1000.csv')
-joined<-semi_join(list1, list2, by = c('x'))
-results<-semi_join(joined, list3, by = c('x'))
-write.csv(results,file = 'results.csv')
+setwd('/home/amp_prog/Desktop/models/GSE242889_liver_cancer')
+list1<-read.csv('pt1.csv')
+list2<-read.csv('pt2.csv')
+list3<-read.csv('pt3.csv')
+list4<-read.csv('pt4.csv')
+list5<-read.csv('pt5.csv')
+#--------------------------------------------------------------------------------
+x<-semi_join(list1, list2, by = c('x'))
+x<-semi_join(x, list3, by = c('x'))
+x<-semi_join(x, list4, by = c('x'))
+x<-semi_join(x, list5, by = c('x'))
+write.csv(x,file = 'gene_list.csv')
 
-###---to see significance of search results
-setwd('/home/amp_prog/Downloads')
-results<-read.csv('RESULTS.csv')
-list<-results$X
-setwd('/home/amp_prog/Downloads')
-write.csv(x,file = 'milr1.CRC.1000.csv')
