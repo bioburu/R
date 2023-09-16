e42e68c32924cc2f36d0a301386600f7cceeaced
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2023-09-15 18:09:10 -1000

    Update Modeler.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- Modeler.R
+++ Modeler.R
@@ -1,5 +1,5 @@
-x<-FindMarkers(data, ident.1 = 'LAC', ident.2 = 'adjacent', 
-               features = c(top1000),logfc.threshold=1,min.pct1=1,
+x<-FindMarkers(data, ident.1 = '', ident.2 = '', 
+               features = c(),logfc.threshold=1,min.pct1=1,
                max.pct2=0.0001,only.pos = TRUE)
-write.csv(x,file = 'lung.pt1.csv')
+write.csv(x,file = '')
 VlnPlot(data, features = c(row.names(x)[1:3]),cols = c('grey','red'))
