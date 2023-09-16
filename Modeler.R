c6d641a341e2503aa22361e4b08c85ce9e1df01f
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2023-09-15 18:28:42 -1000

    Update Modeler.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- Modeler.R
+++ Modeler.R
@@ -1,5 +1,5 @@
 x<-FindMarkers(data, ident.1 = '', ident.2 = '', 
                features = c(),logfc.threshold=1,min.pct1=1,
                max.pct2=0.0001,only.pos = TRUE)
+VlnPlot(data, features = c(row.names(x)[1:12]),cols = c('grey','red'))
 write.csv(x,file = '')
-VlnPlot(data, features = c(row.names(x)[1:3]),cols = c('grey','red'))
