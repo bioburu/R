3230aeb038ecd02f052b1edd66119bfb9c3fd581
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2023-11-21 16:32:02 -0500

    Update bulk.RNA.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- bulk.RNA.R
+++ bulk.RNA.R
@@ -14,6 +14,10 @@ matrix<-read.csv('gene.list_unordered.csv')
 matrix<-matrix[order(matrix$Gene), ]
 row.names(matrix)<-make.names(matrix$Gene,unique = TRUE)
 matrix<-matrix[,-c(1:7)]
+colnames(matrix)[2]<-'Norm_2'
+#remove all none expressing genes
+matrix<-filter(matrix,Norm_1>0,Norm_2>0,Norm_3>0,TAA_1>0,TAA_2>0,TAA_3>0)
+#
 data <- CreateSeuratObject(counts=matrix,project = 'TAA')
 gc()
 data@active.ident
@@ -41,11 +45,12 @@ ElbowPlot(data)
 gc()
 table(data@meta.data$orig.ident)
 #------------------------------------------
-#x<-FindMarkers(data, ident.1 = 'TAA', ident.2 = 'Norm', 
-#               features = c(top1000),logfc.threshold=1,min.pct1=1,
-#               max.pct2=0.0001,only.pos = FALSE)
-x<-FindMarkers(data, ident.1 = 'TAA',ident.2 = 'Norm',features = c(top1000))
-x<-x[-c(215:997),]
+x<-FindMarkers(data, ident.1 = 'TAA', ident.2 = 'Norm', 
+               features = c(top1000),logfc.threshold=1,min.pct1=1,
+               max.pct2=0.0001,only.pos = TRUE,test.use = 'negbinom')
+#x<-FindMarkers(data, ident.1 = 'TAA',ident.2 = 'Norm',
+#               features = c(top1000),test.use='negbinom')
+#x<-x[-c(215:997),]
 x<-cbind(row.names(x),x)
 colnames(x)[1]<-'Gene'
 matrix2<-read.csv('gene.list_unordered.csv')
@@ -55,29 +60,13 @@ matrix2<-cbind(row.names(matrix2),matrix2)
 colnames(matrix2)[1]<-'Gene'
 df<-merge(x,matrix2,by='Gene')
 df<-df[order(df$avg_log2FC, decreasing=TRUE),]
+break
 out<-df
 out$Gene
 out$Gene<-sub('\\..*','',out$Gene)
 out$Gene
-write.csv(out,file = 'most_sig_out.csv')
-
+#write.csv(out,file = 'most_sig_genes.csv')
 break 
-VlnPlot(data, features = c('Slc16a2','Slco1c1','Slco1c1.1','Klf9','Shh','Gli1','Gli2','Atoh1'),cols = c('grey','red'))
-FindMarkers(data, ident.1 = 'TAA', ident.2 = 'Norm', features = c('Slc16a2','Slco1c1','Slco1c1.1','Slco1c1.2','Slco1c1.3','Slco1c1.4','Slco1c1.5','Slco1c1.6','Slco1c1.7','Slco1c1.8'))
-                                                                     
-VlnPlot(data, features = c('Ptch2','Ptch2.1','Ptch2.2','Ptch2.3','Ptch2.4','Ptch2.5'),cols = c('grey','red'))
-FindMarkers(data, ident.1 = 'TAA', ident.2 = 'Norm', features = c('Ezh2','Ezh2.1','Ezh2.2','Ezh2.3','Ezh2.4','Ezh2.5','Ezh2.6','Ezh2.7','Ezh2.8',
-                                                                  'Ezh2.9','Ezh2.10','Ezh2.11','Ezh2.12','Ezh2.14'))
+VlnPlot(data, features = c(row.names(x)[1:12]),cols = c('grey','red'))
+FindMarkers(data, ident.1 = 'TAA', ident.2 = 'Norm', features = c(row.names(x)[1:12]),test.use = 'negbinom')
 
-
-x2<-cbind(row.names(x),x)
-colnames(x2)[1]<-'Gene'
-x2<-x2[,-c(4:6)]
-df<-merge(x2,matrix2,by='Gene')
-df<-df[order(df$avg_log2FC, decreasing=TRUE),]
-#-----output dataframe of most FC + sig genes
-out<-df
-out$Gene
-out$Gene<-sub('\\..*','',out$Gene)
-out$Gene
-#write.csv(out,file = 'most_sig_out.csv')
