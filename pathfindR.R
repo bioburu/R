3d71dba1e5eb1d454f82b691094a7f54f71831d7
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2023-11-22 13:38:00 -0500

    Update pathfindR.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- pathfindR.R
+++ pathfindR.R
@@ -6,14 +6,10 @@ install.packages("magick")
 BiocManager::install("KEGGgraph")
 install.packages('pathfindR')
 library(pathfindR)
-#----obtain mouse specific gene sets
-#gsets_list<-get_gene_sets_list(source = 'mmu_KEGG')
-setwd('/home/deviancedev/Desktop/drive_nov2023/FCCC/alignments/finals')
+setwd('/home/deviancedev/Desktop/drive_nov2023/FCCC/alignments/finals/results')
 matrix<-read.csv('DESeq2_out.csv')
-matrix<-matrix[order(matrix$p_val_adj), ]
-matrix<-matrix[,-c(1,3,5:7)]
-#------clean gene set
-#genes<-input_processing(input = matrix,p_val_threshold = 0.01,pin_name_path = 'KEGG')
+#matrix<-matrix[order(matrix$p_val_adj), ]
+matrix<-matrix[,-c(1,3,5)]
 #------get gene sets
 gene_list <- fetch_gene_set(
   gene_sets = "mmu_KEGG",
@@ -25,9 +21,7 @@ head(mmu_kegg_gsets)
 #------------------------------------------------------------
 mmu_kegg_descriptions <- gene_list[[2]]
 head(mmu_kegg_descriptions)
-#---
-break
-matrix<-matrix[,-2]
+#matrix<-matrix[,-2]
 test<-run_pathfindR(
   input = matrix,
   convert2alias = FALSE,
@@ -35,5 +29,16 @@ test<-run_pathfindR(
   custom_descriptions = mmu_KEGG_descriptions,
   pin_name_path = 'mmu_STRING'
 )
-cluster_enriched_terms(test)
-visualize_terms(test)
+data<-cluster_enriched_terms(test)
+term_gene_graph(test)
+term_gene_graph(test,num_terms = c(5),use_description = TRUE)
+enrichment_chart(test, plot_by_cluster = TRUE)
+
+#write.csv(data,file = 'mmu_KEGG_results.csv')
+break 
+#----for individual cluster plots
+visualize_terms(result_df = data,
+                hsa_KEGG = FALSE,
+                pin_name_path = 'mmu_STRING')
+
+
