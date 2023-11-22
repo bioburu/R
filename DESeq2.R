332de0e45e4309bf4e2469fd5edb642eb3ce4c6f
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2023-11-21 21:00:01 -0500

    Create DESeq2.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

---
+++ DESeq2.R
@@ -0,0 +1,32 @@
+library(DESeq2)
+setwd('/home/deviancedev/Desktop/drive_nov2023/FCCC/alignments/finals')
+matrix<-read.csv('gene.list_ordered.csv')
+matrix<-matrix[,-c(2:7)]
+row.names(matrix)<-make.names(matrix$Gene,unique=TRUE)
+matrix<-matrix[,-1]
+colnames(matrix)[2]<-'Norm_2'
+head(matrix)
+str(matrix)
+matrix<-round(matrix)
+#-------------------------------------------------------------------------------
+names<-c('Norm_1','Norm_2','Norm_3','TAA_1','TAA_2','TAA_3')
+condition<-c('Normal','Normal','Normal','Tumor','Tumor','Tumor')
+type<-c('paired','paired','paired','paired','paired','paired')
+coldata<-data.frame(cbind(names,condition,type))
+row.names(coldata)<-coldata$names
+coldata<-coldata[,-1]
+head(coldata)
+#-----------------------------------------------------------------------------
+deseq<-DESeqDataSetFromMatrix(countData = matrix,colData = coldata,design = ~ condition)
+deseq
+DE<-DESeq(deseq)
+#-------------------------------------------------------------------------------
+results<-results(DE)
+results
+df<-data.frame(results)
+df<-df[order(df$pvalue, decreasing=FALSE),]
+df<-cbind(row.names(df),df)
+df$`row.names(df)`<-sub('\\..*','',df$`row.names(df)`)
+colnames(df)[1]<-'Gene'
+df<-df[-c(20328:115163),]
+write.csv(df,file = 'DESeq2_out.csv')
