e5c5707891b0e92cefe008533ce21de01353cbf9
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-01-09 15:53:22 -0500

    Update BiomaRt_template.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- BiomaRt_template.R
+++ BiomaRt_template.R
@@ -1,35 +1,50 @@
 library(biomaRt)
-setwd()
-matrix <- read.csv('')
+library(dplyr)
+setwd('/home/deviancedev/Desktop/drive_nov2023/FCCC/RNAseq_exosomes/run_2')
+matrix <- read.csv('exosomes_run2.csv')
+matrix<-cbind(matrix$Gene,matrix)
+matrix$`matrix$Gene`
+matrix$`matrix$Gene`<-sub('\\..*','',matrix$`matrix$Gene`)
+matrix$`matrix$Gene`
 #---set host to https://useast.ensembl.org or https://uswest.ensembl.org
 #---------------------------------------------------------------------
 Sys.setenv("http_proxy"="http://my.proxy.org:9999")
 listMarts()
 ensembl=useMart("ensembl")
 listDatasets(ensembl)
-ensembl=useDataset("hsapiens_gene_ensembl",mart = ensembl)
+ensembl=useDataset("mmusculus_gene_ensembl",mart = ensembl)
 filters = listFilters(ensembl)
 attributes=listAttributes(ensembl)
 #-----------------------------------------------------------------
 dim(matrix)
-geneid <- matrix$Gene
+geneid <- matrix$`matrix$Gene`
 head(geneid)
 dim(geneid)
 listFilters(ensembl)
 listAttributes(ensembl)
-genes <-getBM(attributes = c('ensembl_transcript_id_version','external_gene_name'),
-      filters = 'ensembl_transcript_id_version',
-      values = geneid,
-      mart = ensembl)
+genes <-getBM(attributes = c('ensembl_transcript_id','external_gene_name','chromosome_name','p_value','start_position','end_position','description'),
+              filters = 'ensembl_transcript_id',
+              values = geneid,
+              mart = ensembl)
 head(genes)
-df <- matrix[matrix$Gene %in% unique(genes$ensembl_transcript_id_version),]
+df <- matrix[matrix$`matrix$Gene` %in% unique(genes$ensembl_transcript_id),]
 dim(df)
 dim(matrix)
-genes$ensembl_transcript_id_version
-colnames(genes)[1]<- 'Gene'
-Ftable <- merge(genes, df, by="Gene")
+genes$ensembl_transcript_id
+colnames(genes)[1]<- 'matrix$Gene'
+Ftable <- merge(genes, df, by="matrix$Gene")
 Ftable<- Ftable[,-c(1)]
+colnames(Ftable)[7] <- c('transcript_version')
 colnames(Ftable)[1] <- c('Gene')
-View(Ftable)
-write.csv(Ftable,file = 'relapse_2.csv')
+head(Ftable)
+matrix<-data.frame(Ftable)
+#------Remove duplicate genes by p_value
+matrix<-matrix%>%
+  group_by(Gene)%>%
+  arrange(p_value)%>%
+  slice(1)
+matrix<-data.frame(matrix)
+str(matrix)
+break
+write.csv(matrix,file = 'exosomes_rnaseq_run2.csv')
 
