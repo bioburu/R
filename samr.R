7347632fb22408898e921f9f114ec46ff9dd0cfe
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-01-20 11:45:36 -0500

    Update samr.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- samr.R
+++ samr.R
@@ -1,3 +1,12 @@
 ERROR: dependencies ‘impute’, ‘shinyFiles’, ‘openxlsx’, ‘GSA’ are not available for package ‘samr’
 * removing ‘/home/deviancedev/R/x86_64-pc-linux-gnu-library/4.1/samr’
 
+BiocManager::install('impute')
+library(impute)
+install.packages('shinyFiles')
+library(shinyFiles)
+install.packages('openxlsx')
+library(openxlsx)
+install.packages('GSA')
+library('GSA')
+#---now install samr from .tar.gz
