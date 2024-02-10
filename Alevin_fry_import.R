46886bf0a8bbb004b93dedd354dff6591c2649ac
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-02-10 10:03:47 -0500

    Update Alevin_fry_import.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- Alevin_fry_import.R
+++ Alevin_fry_import.R
@@ -4,13 +4,14 @@ library(SummarizedExperiment)
 library(biomaRt)
 library(dplyr)
 library(Seurat)
-files<-'/home/deviancedev/Desktop/drive_jan2024/Alevin_Salmon/af_tutorial/alevin_pilot_quant'
+files<-'/home/deviancedev/Desktop/drive_jan2024/FCCC/scRNAseq_test/SRR27442073/mm10_quant'
 x<-loadFry(files,outputFormat = 'scRNA',nonzero = FALSE,quiet = FALSE)
 x
 #----
 gc()
 counts <- assay(x, "counts")
 counts
+dim(counts)
 df<-data.frame(counts)
 gc()
 #------
@@ -22,7 +23,9 @@ Sys.setenv("http_proxy"="http://my.proxy.org:9999")
 listMarts()
 ensembl=useMart("ensembl")
 listDatasets(ensembl)
-ensembl=useDataset("hsapiens_gene_ensembl",mart = ensembl)
+ensembl<-useEnsembl(biomart = 'genes',
+                   dataset = 'mmusculus_gene_ensembl',
+                   version=102)
 filters = listFilters(ensembl)
 attributes=listAttributes(ensembl)
 #-----------------------------------------------------------------
@@ -37,6 +40,7 @@ genes <-getBM(attributes = c('ensembl_gene_id','external_gene_name','chromosome_
               values = geneid,
               mart = ensembl)
 head(genes)
+genes<-genes[!duplicated(genes$ensembl_gene_id), ]
 df <- matrix[matrix$Gene %in% unique(genes$ensembl_gene_id),]
 dim(df)
 dim(matrix)
@@ -44,6 +48,9 @@ colnames(genes)[1]<- 'Gene'
 Ftable <- merge(genes, df, by="Gene")
 gc()
 colnames(Ftable)[1] <- c('ensembl_id')
+rm(attributes,counts,df,ensembl,filters,genes,matrix,x)
+gc()
+DF<-Ftable[!duplicated(Ftable$external_gene_name), ]
 break
-setwd('/home/deviancedev/Desktop/drive_jan2024/Alevin_Salmon/af_tutorial')
-write.csv(Ftable,file = 'alevin_pbmc_scRNAseq.csv')
+setwd('/home/deviancedev/Desktop/drive_jan2024/FCCC/scRNAseq_test/SRR27442073')
+write.csv(Ftable,file = 'GSM8004749.Sciatic.nerve.CD45p.spon.autoimmune.neuropathy.csv')
