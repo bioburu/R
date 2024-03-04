7597efad5ba0ddae24cf741d2fbe6e1488902e9f
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-03-03 19:18:33 -0500

    Update scPipe.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- scPipe.R
+++ scPipe.R
@@ -1,8 +1,9 @@
 library(scPipe)
-output_folder <- '/home/em_b/Desktop/SRR22324456/output_folder'
-r1<-file.path('/home/em_b/Desktop/SRR22324456/SRR22324456_S1_L001_R1_001.fastq.gz') 
-r2<-file.path('/home/em_b/Desktop/SRR22324456/SRR22324456_S1_L001_R2_001.fastq.gz') 
-r3<-file.path('/home/em_b/Desktop/SRR22324456/SRR22324456_S1_L001_R3_001.fastq.gz')
+output_folder <- '/home/em_b/Desktop/FCCC/SRR22324456/output_folder2'
+r1<-file.path('/home/em_b/Desktop/FCCC/SRR22324456/SRR22324456_S1_L001_R1_001.fastq.gz') 
+r2<-file.path('/home/em_b/Desktop/FCCC/SRR22324456/SRR22324456_S1_L001_R2_001.fastq.gz') 
+r3<-file.path('/home/em_b/Desktop/FCCC/SRR22324456/SRR22324456_S1_L001_R3_001.fastq.gz')
+cat('No valid barcode can be ignored')
 sc_atac_trim_barcode (r1            = r1, 
                       r2            = r2, 
                       bc_file       = r3, 
@@ -14,7 +15,7 @@ demux_r2<-file.path(output_folder, "demux_completematch_SRR22324456_S1_L001_R2_0
 aligned_bam<-sc_aligning(ref = '/home/em_b/cellranger/scatacseq/refdata-cellranger-arc-mm10-2020-A-2.0.0/fasta/genome.fa', 
                 R1 = demux_r1, 
                 R2 = demux_r2, 
-                nthreads  = 20,
+                nthreads  = 24,
                 output_folder = output_folder)
 sorted_tagged_bam <- sc_atac_bam_tagging (inbam = aligned_bam, 
                        output_folder =  output_folder, 
@@ -25,6 +26,7 @@ if (!isFALSE(removed))
   sorted_tagged_bam <- removed
 sc_atac_create_fragments(inbam = sorted_tagged_bam,
                          output_folder = output_folder)
+break 
 cat('macs3 callpeak -f BAMPE -t demux_completematch_aligned_tagged_sorted_markdup.bam -g mm -n name_me -B -q 0.01 --call-summits')
 features<-file.path('/home/em_b/Desktop/SRR22324456/output_folder/SRR22324456_peaks.narrowPeak')
 fragments<-file.path('/home/em_b/Desktop/SRR22324456/output_folder/fragments.bed')
@@ -42,16 +44,16 @@ sc_atac_feature_counting (fragment_file = fragments,
                           output_folder = output_folder,
                           fix_chr       = "none"
                           )
-feature_matrix <- readRDS(file.path("/home/em_b/Desktop/SRR22324456/output_folder/unfiltered_feature_matrix.rds"))
-dplyr::glimpse(feature_matrix)
-sparseM <- readRDS(file.path("/home/em_b/Desktop/SRR22324456/output_folder/sparse_matrix.rds"))
-dplyr::glimpse(sparseM)
 sce <- sc_atac_create_sce(input_folder = output_folder,
                    organism     = "mm10",
                    feature_type = "peak",
                    pheno_data   = NULL,
                    report       = FALSE)
-data<-readRDS(file.path('/home/em_b/Desktop/SRR22324456/output_folder/scPipe_atac_SCEobject.rds'))
-df<-data.frame(as.matrix(counts(data)))
-#----make fragments.tsv file from fragments.bed
-break 
+#sort -k1,1 -k2,2n -k3,3n fragments.bed >fragments.sorted.bed
+cat('convert fragment.bed to .tsv.gz and .tsv.gz.tbi files and place into final_output')
+cat('mkdir final_output')
+cat('cd final_output')
+cat('sort -k1,1 -k2,2n -k3,3n fragments.bed >fragments.sorted.bed')
+cat('bgzip fragments.sorted.bed')
+cat('tabix -p bed fragments.sorted.bed.gz')
+cat('convert all bed files to tsv')
