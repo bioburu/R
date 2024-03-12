facb97ba6338c47e0e0dd4490ddc5eceeefee1cd
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-03-12 13:18:03 -0400

    Update Hi-C_bulk.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- Hi-C_bulk.R
+++ Hi-C_bulk.R
@@ -1,1 +1,3 @@
 https://bioconductor.org/books/devel/OHCA/pages/visualization.html
+
+https://www.bioinformatics.babraham.ac.uk/projects/hicup/read_the_docs/html/index.html#processing-the-test-dataset
