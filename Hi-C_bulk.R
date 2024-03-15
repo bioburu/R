ef113a14036db1b26c5eb1dc683b70c04cdf48be
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-03-14 21:23:43 -0400

    Update and rename Hi-C_bulk.R to HiContact.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- Hi-C_bulk.R
+++
@@ -1,3 +0,0 @@
-https://bioconductor.org/books/devel/OHCA/pages/visualization.html
-
-https://www.bioinformatics.babraham.ac.uk/projects/hicup/read_the_docs/html/index.html#processing-the-test-dataset
