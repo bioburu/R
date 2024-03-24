5016d43fdf5712bf144d4a389af5b70c302f1637
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-03-24 18:34:32 -0400

    Update hicool.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- hicool.R
+++ hicool.R
@@ -3,33 +3,17 @@ library(HiCool)
 library(HiContacts)
 library(HiCcompare)
 library(HiCDOC)
-setwd('/home/em_b/Desktop/hicool/SRR22161720.musC8.didnt work')
-r1<-file.path('SRR22161720_1.fastq')
-#--files are too big to load into environment
-#x<-readFastq(r1)
-#x
-#head(x@sread)
-r2<-file.path('SRR22161720_2.fastq')
-#x2<-readFastq(r2)
-#x2
-#head(x2@sread)
-#HiCool(r1,
-#       r2,
-#       restriction = 'DpnII',
-#       genome = 'mm10',
-#       threads = 24,
-#       exclude_chr = 'Mito|chrM|MT',
-#       output = './HiCool_output/',
-#       keep_bam = TRUE,
-#       build_report = TRUE)
-HiCool(
+setwd('/home/em_b/Desktop/hicool/SRR22161720')
+r1<-file.path('SRR22161720_1.part_001.fastq.gz')
+r2<-file.path('SRR22161720_2.part_001.fastq.gz')
+hcf <- HiCool(
   r1 = r1, 
   r2 = r2, 
   restriction = 'MboI', 
-  #resolutions = c(1000,4000,8000,16000), 
+  resolutions = c(1000,4000, 8000, 16000), 
   genome = 'mm10', 
   output = './HiCool/',
-  threads = 24,
   keep_bam = TRUE,
   build_report = TRUE)
-break 
+#-----------------------------------
+#-----Arima are DpnII,HinfI.
