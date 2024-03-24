518929e079ff382cbb9fc1a89cf0543e9cc2aa51
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-03-24 13:32:00 -0400

    Update hicool.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- hicool.R
+++ hicool.R
@@ -3,32 +3,33 @@ library(HiCool)
 library(HiContacts)
 library(HiCcompare)
 library(HiCDOC)
-setwd('/home/em_b/Desktop/hicool/SRR21038697/550mil')
-r1<-file.path('SRR21038697filtered_R1.part_001.fastq.gz')
+setwd('/home/em_b/Desktop/hicool/SRR22161720.musC8.didnt work')
+r1<-file.path('SRR22161720_1.fastq')
 #--files are too big to load into environment
 #x<-readFastq(r1)
 #x
 #head(x@sread)
-r2<-file.path('SRR21038697filtered_R2.part_001.fastq.gz')
+r2<-file.path('SRR22161720_2.fastq')
 #x2<-readFastq(r2)
 #x2
 #head(x2@sread)
-HiCool(r1,
-       r2,
-       restriction = 'DpnII,HinfI',
-       genome = 'mm10',
-       threads = 22,
-       exclude_chr = 'Mito|chrM|MT',
-       output = './HiCool_output/',
-       keep_bam = TRUE,
-       build_report = TRUE,
-       resolutions = c(1000,4000,8000,16000))
+#HiCool(r1,
+#       r2,
+#       restriction = 'DpnII',
+#       genome = 'mm10',
+#       threads = 24,
+#       exclude_chr = 'Mito|chrM|MT',
+#       output = './HiCool_output/',
+#       keep_bam = TRUE,
+#       build_report = TRUE)
+HiCool(
+  r1 = r1, 
+  r2 = r2, 
+  restriction = 'MboI', 
+  #resolutions = c(1000,4000,8000,16000), 
+  genome = 'mm10', 
+  output = './HiCool/',
+  threads = 24,
+  keep_bam = TRUE,
+  build_report = TRUE)
 break 
-
-try
-python3 -m pip install --user pipx
-python3 -m pipx ensurepath 
-pipx install chromosight 
-chromosight -h 
-
-chromosight detect c241d99d5e2_7833^mapped-R64-1-1^LYRTWZ.mcool::resolutions/1000 results
