e68595770799622510f3ac03faafe309f7dc2c75
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-03-26 12:18:00 -0400

    Update transfers.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- transfers.R
+++ transfers.R
@@ -12,23 +12,151 @@ library(ggplot2)
 library(patchwork)
 library(GenomicRanges)
 library(rtracklayer)
-setwd('/Users/burudpc/Desktop')
-cf<-CoolFile('twofiles^mapped-mm10^CCP6TK.mcool')
-pairs_file<- PairsFile('/Users/burudpc/Desktop/twofiles^mapped-mm10^CCP6TK.pairs')
+library(org.Mm.eg.db)
+library(regioneR)
+library(EnsDb.Mmusculus.v79)
+library(ChIPpeakAnno)
+library(tibble)
+setwd('/Users/burudpc/Desktop/hi-C_crap')
+cf<-CoolFile('SRR22161720_muCD8_MboI_filtered^mapped-mm10^ZOXFW7.mcool')
+pairs_file<- PairsFile('SRR22161720_muCD8_MboI_filtered^mapped-mm10^ZOXFW7.pairs')
 availableResolutions(cf)
 hic<-import(cf,
-            resolution=16000,
+            resolution=1000,
             pairs=pairs_file,
-            format = 'mcool')
+            format = 'mcool',
+            focus=c('chr11:115496001-118000000'))
+hic<-normalize(hic)
+hic <- detrend(hic)
 hic
-#---annotate A/B compartments
+hic<-HiCool::getLoops(hic,resolution=16000)
+hic
+metadata(hic)$chromosight_args
+loops<-topologicalFeatures(hic,'loops')
+loops
+loops<-loops[loops$score>=0.4&loops$qvalue<=1e-6]
+loops
+#GenomicInteractions::export.bedpe(loops,'loops.bedpe')
+#hic<-zoom(hic,1000)
+hic
+hic<-getDiamondInsulation(hic,BPPARAM = BiocParallel::bpparam())
+hic
+hic<-getBorders(hic,weak_threshold = 0.2,strong_threshold = 0.5)
+hic
+borders<-topologicalFeatures(hic,'borders')
+borders
 phasing_track<-BSgenome.Mmusculus.UCSC.mm10::BSgenome.Mmusculus.UCSC.mm10
-compts<-getCompartments(hic,chromosomes = 'chr1',genome = phasing_track)
-compts
-#compartdf<-data.frame(topologicalFeatures(compts,'compartments'))
-plotSaddle(compts)
-metadata(compts)$eigens
-coverage(metadata(compts)$eigens, weight = 'eigen') |> export('hic_eigen.bw')
-topologicalFeatures(compts, "compartments") |> export('hic_compartments.gff3')
+hic<-getCompartments(hic,
+                     chromosomes = 'chr11',
+                     genome = phasing_track,
+                     resolution=16000)
+hic
+plotMatrix(hic,
+           use.scores = 'balanced',
+           scale = 'log10',
+           #limits = c(-1, 1),
+           #cmap = bwrColors(),
+           caption = TRUE,
+           loops=loops,
+           borders=borders,
+           symmetrical=TRUE,
+           rasterize=TRUE)
+loopsdf<-data.frame(topologicalFeatures(hic,'loops'))
+loopsdf<-loopsdf[order(loopsdf$qvalue, decreasing=FALSE),]
+annoData<-toGRanges(EnsDb.Mmusculus.v79)
+annoData
+seqlevelsStyle(loops) <- seqlevelsStyle(annoData)
+colnames(loopsdf)[1:3]<-c('seqnames','start','end')
+Loops1 <- toGRanges(loopsdf)
+anno<-annotatePeakInBatch(Loops1,AnnotationData = annoData)
+anno <- addGeneIDs(anno, orgAnn="org.Mm.eg.db", 
+                   feature_id_type="ensembl_gene_id",
+                   IDs2Add=c("symbol"))
+anno
+Loops1<-data.frame(Loops1)
+Loops1<-add_column(Loops1, frag1_genes=anno$symbol, .after = 'seqnames')
+#-------------------------------------------------------------------------------
+colnames(loopsdf)[c(1:3)]<-c('seqnames1','start1','end1')
+colnames(loopsdf)[c(6:8)]<-c('seqnames','start','end')
+Loops2 <- toGRanges(loopsdf)
+anno<-annotatePeakInBatch(Loops2,AnnotationData = annoData)
+anno <- addGeneIDs(anno, orgAnn="org.Mm.eg.db", 
+                   feature_id_type="ensembl_gene_id",
+                   IDs2Add=c("symbol"))
+anno
+anno$symbol
+loop_comparison<-add_column(Loops1, frag2_genes=anno$symbol, .after = 'seqnames2')
 break 
-compts<-autocorrelate(compts)
+cowplot::plot_grid(
+  plotMatrix(hic, use.scores = 'count', caption = FALSE),
+  plotMatrix(hic, use.scores = 'balanced', caption = FALSE),
+  plotMatrix(hic, use.scores = 'ICE', caption = FALSE), 
+  nrow = 1)
+cowplot::plot_grid(
+  plotMatrix(hic, use.scores = 'balanced', caption = FALSE),
+  plotMatrix(hic, use.scores = 'ICE', caption = FALSE),
+  plotMatrix(hic, use.scores = 'expected', caption = FALSE),
+  plotMatrix(hic, use.scores = 'detrended', caption = FALSE), 
+  nrow = 2)
+cowplot::plot_grid(
+  plotMatrix(hic, use.scores = 'balanced', scale = 'log10', limits = c(-3.5, -1.2), caption = FALSE),
+  plotMatrix(hic, use.scores = 'ICE', scale = 'log10', limits = c(-3.5, -1.2), caption = FALSE),
+  plotMatrix(hic, use.scores = 'detrended', scale = 'linear', limits = c(-1, 1), cmap = bwrColors(), caption = FALSE), 
+  nrow = 1)
+plotMatrix(hic,
+           loop=loops,
+           limits=c(-4,-1.2),
+           caption=TRUE)
+plotMatrix(hic,
+           use.scores = 'detrended',
+           scale = 'linear',
+           limits = c(-1, 1),
+           cmap = bwrColors(),
+           caption = FALSE,
+           loops=loops)
+plotMatrix(hic,
+           use.scores = 'detrended',
+           scale = 'linear',
+           limits = c(-1, 1),
+           cmap = bwrColors(),
+           caption = FALSE,
+           loops=loops,
+           borders=borders)
+#hic<-autocorrelate(hic)
+plotSaddle(hic)
+break 
+hic
+topologicalFeatures(hic,'compartments')
+metadata(hic)$eigens
+coverage(metadata(hic)$eigens, weight = 'eigen') |> export('hic_eigen.bw')
+topologicalFeatures(hic, "compartments") |> export('hic_compartments.gff3')
+hic<-autocorrelate(hic)
+hic
+p1 <- plotMatrix(hic,
+                 use.scores = 'autocorrelated',
+                 scale = 'linear',
+                 limits = c(-1, 1),
+                 caption = FALSE)
+p1
+eigen <- coverage(metadata(hic)$eigens, weight = 'eigen')[4]
+eigen
+x<-cumsum(runLength(eigen))
+x
+y<-runValue(eigen)
+y
+eigen_df <- data.frame(pos = x, eigen = y)
+eigen_df<-eigen_df[,-c(1:2,4:5)]
+head(eigen_df)
+#-----------------------------------------------------------------------------
+str(eigen_df)
+barplot(eigen_df$eigen.value)
+plot(eigen_df)#,aes=c(x=eigen_df$pos.value,y=eigen_df$eigen.value))
+p1
+wrap_plots(p1, p2, ncol = 1, heights = c(10, 1))
+p2 <- ggplot(eigen_df, aes(x = pos, y = eigen)) + 
+  geom_area() + 
+  theme_void() + 
+  coord_cartesian(expand = TRUE) + 
+  labs(x = "Genomic position", y = "Eigenvector value")
+p2
+wrap_plots(p1, p2, ncol = 1, heights = c(10, 1))
