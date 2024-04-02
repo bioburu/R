460efdefe2f132d52c430ccab919c239b85c5aa6
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-04-02 11:28:02 -0400

    Update hicontacts.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- hicontacts.R
+++ hicontacts.R
@@ -1,11 +1,9 @@
-#library(ShortRead)
 library(HiCool)
 library(HiContacts)
 library(GenomicRanges)
 library(HiCExperiment)
 library(BiocParallel)
-#library(HiCcompare)
-#library(HiCDOC)
+library(HiCcompare)
 library(WGCNA)
 library(ggplot2)
 library(patchwork)
@@ -17,35 +15,122 @@ library(EnsDb.Mmusculus.v79)
 library(ChIPpeakAnno)
 library(tibble)
 library(pathfindR)
-setwd('/home/em_b/Desktop/hicool/SRR26855487_il4_macro_DpnII_HinfI/HiCool')
-cf<-CoolFile('/home/em_b/Desktop/hicool/SRR26855487_il4_macro_DpnII_HinfI/HiCool/matrices/SRR26855487_filtered^mapped-mm10^B3IIMF.mcool')
-pairs_file<- PairsFile('/home/em_b/Desktop/hicool/SRR26855487_il4_macro_DpnII_HinfI/HiCool/pairs/SRR26855487_filtered^mapped-mm10^B3IIMF.pairs')
-availableResolutions(cf)
+setwd('/home/em_b/Desktop/hicool/GSE248029_il4_macro_DpnII_HinfI/mm10_output')
 #-------------------------------------------------------------------------------
-hic<-import(cf,
+BL6_cf<-CoolFile('/home/em_b/Desktop/hicool/GSE248029_il4_macro_DpnII_HinfI/mm10_output/matrices/SRR26855486_BL6.mcool')
+BALBc_cf<-CoolFile('/home/em_b/Desktop/hicool/GSE248029_il4_macro_DpnII_HinfI/mm10_output/matrices/SRR26855487_BALBc.mcool')
+availableResolutions(BL6_cf)
+availableResolutions(BALBc_cf)
+#-------------------------------------------------------------------------------
+BL6_pairs<- PairsFile('/home/em_b/Desktop/hicool/GSE248029_il4_macro_DpnII_HinfI/mm10_output/pairs/SRR26855486_BL6.pairs')
+BALBc_pairs<- PairsFile('/home/em_b/Desktop/hicool/GSE248029_il4_macro_DpnII_HinfI/mm10_output/pairs/SRR26855487_BALBc.pairs')
+#-------------------------------------------------------------------------------
+BL6_hic<-import(BL6_cf,
             resolution=1000,
-            pairs=pairs_file,
+            pairs=BL6_pairs,
             format = 'mcool',
-            focus=c('chr11:115496001-118000000'))
-hic<-normalize(hic)
-hic <- detrend(hic)
-hic<-HiCool::getLoops(hic,resolution=16000)
+            focus=c('chr12:24000000-28000000'))
+BALBc_hic<-import(BALBc_cf,
+                resolution=1000,
+                pairs=BALBc_pairs,
+                format = 'mcool',
+                focus=c('chr12:24000000-28000000'))
+gc()
+#cowplot::plot_grid(
+#  plotMatrix(BL6_hic,
+#             use.scores = 'balanced',
+#             #scale = 'log10',
+##             limits = c(-4, -1),
+#             loops=BL6_loops,
+#             borders=BL6_borders),
+#  plotMatrix(BALBc_hic,
+#             use.scores = 'balanced',
+#             #scale = 'log10',
+#             limits = c(-4, -1),
+#             loops=BALBc_loops,
+#             borders=BALBc_borders),
+#  nrow = 1)
+#-------------------------------------------------------------------------------
+BL6_hic<-normalize(BL6_hic)
+BALBc_hic<-normalize(BALBc_hic)
+#-------------------------------------------------------------------------------
+BL6_hic <- detrend(BL6_hic)
+BALBc_hic <- detrend(BALBc_hic)
+#-------------------------------------------------------------------------------
+BL6_hic<-autocorrelate(BL6_hic)
+BALBc_hic<-autocorrelate(BALBc_hic)
+summary(scores(BL6_hic, 'autocorrelated'))
+summary(scores(BALBc_hic, 'autocorrelated'))
+#-------------------------------------------------------------------------------
+BL6_hic <- despeckle(BL6_hic)
+BALBc_hic <- despeckle(BALBc_hic)
+gc()
+#-------------------------------------------------------------------------------
+setwd('/home/em_b/Desktop/hicool/GSE248029_il4_macro_DpnII_HinfI/mm10_output/BL6_loops')
+BL6_hic<-HiCool::getLoops(BL6_hic,resolution=16000)
+setwd('/home/em_b/Desktop/hicool/GSE248029_il4_macro_DpnII_HinfI/mm10_output/BALBc_loops')
+BALBc_hic<-HiCool::getLoops(BALBc_hic,resolution=16000)
 #metadata(hic)$chromosight_args
-loops<-topologicalFeatures(hic,'loops')
+BL6_loops<-topologicalFeatures(BL6_hic,'loops')
+BALBc_loops<-topologicalFeatures(BALBc_hic,'loops')
 #View(data.frame(loops))
-loops<-loops[loops$score>=0.4&loops$qvalue<=1e-6]
-#GenomicInteractions::export.bedpe(loops,'loops.bedpe')
+BL6_loops<-BL6_loops[BL6_loops$score>=0.4&BL6_loops$qvalue<=1e-6]
+BALBc_loops<-BALBc_loops[BALBc_loops$score>=0.4&BALBc_loops$qvalue<=1e-6]
+setwd('/home/em_b/Desktop/hicool/GSE248029_il4_macro_DpnII_HinfI/mm10_output')
+#GenomicInteractions::export.bedpe(BL6_loops,'loops.bedpe')
 #hic<-zoom(hic,1000)
-hic<-getDiamondInsulation(hic,BPPARAM = BiocParallel::bpparam())
-hic<-getBorders(hic,weak_threshold = 0.2,strong_threshold = 0.5)
-borders<-topologicalFeatures(hic,'borders')
+
+#-------------------------------------------------------------------------------
+BL6_hic<-getDiamondInsulation(BL6_hic,BPPARAM = BiocParallel::bpparam())
+BALBc_hic<-getDiamondInsulation(BALBc_hic,BPPARAM = BiocParallel::bpparam())
+BL6_hic<-getBorders(BL6_hic,weak_threshold = 0.2,strong_threshold = 0.5)
+BALBc_hic<-getBorders(BALBc_hic,weak_threshold = 0.2,strong_threshold = 0.5)
+BL6_borders<-topologicalFeatures(BL6_hic,'borders')
+BALBc_borders<-topologicalFeatures(BALBc_hic,'borders')
 #View(data.frame(borders))
+#-------------------------------------------------------------------------------
 phasing_track<-BSgenome.Mmusculus.UCSC.mm10::BSgenome.Mmusculus.UCSC.mm10
-hic<-getCompartments(hic,
-                     chromosomes = 'chr11',
+BL6_hic<-getCompartments(BL6_hic,
+                     chromosomes = 'chr12',
                      genome = phasing_track,
                      resolution=16000)
-hic
+BALBc_hic<-getCompartments(BALBc_hic,
+                         chromosomes = 'chr12',
+                         genome = phasing_track,
+                         resolution=16000)
+BL6_hic
+BALBc_hic
+gc()
+#-------------------------------------------------------------------------------
+BL6_ps <- distanceLaw(BL6_pairs)
+BL6_ps$sample <- 'BL6_il4_macrophages'
+#bl6_ps<-mutate(BL6_ps,sample='BL6_il4_macrophages')
+gc()
+BALBc_ps <- distanceLaw(BALBc_pairs)
+BALBc_ps$sample <- 'BALBc_il4_macrophages'
+#balbc_ps<-mutate(BALBc_ps,sample='BALBc_il4_macrophages')
+gc()
+plot_ps<-rbind(BL6_ps,BALBc_ps)
+plot_ps
+plotPs(plot_ps, mapping=ggplot2::aes(x = binned_distance, y = norm_p,color=sample))
+#plotPsSlope(plot_ps, mapping=ggplot2::aes(x = binned_distance, y = slope,color=sample))
+gc()
+#-------------------------------------------------------------------------------
+cowplot::plot_grid(
+  plotMatrix(BL6_hic,
+             use.scores = 'balanced.despeckled',
+             scale = 'log10',
+             limits = c(-4, -1),
+             loops=BL6_loops,
+             borders=BL6_borders),
+  plotMatrix(BALBc_hic,
+             use.scores = 'balanced.despeckled',
+             scale = 'log10',
+             limits = c(-4, -1),
+             loops=BALBc_loops,
+             borders=BALBc_borders),
+  nrow = 1)
+break 
 #-------------------------------------------------------------------------------
 loopsdf<-data.frame(topologicalFeatures(hic,'loops'))
 loopsdf<-loopsdf[order(loopsdf$qvalue, decreasing=FALSE),]
@@ -62,84 +147,6 @@ anno <- addGeneIDs(anno, orgAnn="org.Mm.eg.db",
 Loops1<-data.frame(Loops1)
 Loops1<-add_column(Loops1, frag1_genes=anno$symbol, .after = 'seqnames')
 #-------------------------------------------------------------------------------
-gc()
-hic<-autocorrelate(hic)
-hic
-summary(scores(hic, 'autocorrelated'))
-ps <- distanceLaw(hic)
-ps
-plotPs(ps, ggplot2::aes(x = binned_distance, y = norm_p))
-cat('plot shows contact frequencies are more abundant at shorter distances')
-plotPsSlope(ps, ggplot2::aes(x = binned_distance, y = slope))
-cat('this plot is useful if comparing two conditions and overlaying')
-gc()
-cowplot::plot_grid(
-  #  plotMatrix(hic, 
-  #             use.scores = 'count',
-  #             caption = FALSE,
-  #             loops=loops,
-  #             borders=borders,
-  #             symmetrical=TRUE,
-  #             rasterize=TRUE),
-  plotMatrix(hic,
-             use.scores = 'balanced',
-             caption = FALSE,
-             loops=loops,
-             borders=borders,
-             symmetrical=TRUE,
-             rasterize=TRUE),
-  plotMatrix(hic,
-             use.scores = 'ICE',
-             caption = FALSE,
-             loops=loops,
-             borders=borders,
-             symmetrical=TRUE,
-             rasterize=TRUE),
-  plotMatrix(hic,
-             use.scores = 'expected',
-             caption = FALSE,
-             loops=loops,
-             borders=borders,
-             symmetrical=TRUE,
-             rasterize=TRUE),
-  plotMatrix(hic,
-             use.scores = 'detrended',
-             caption = FALSE,
-             loops=loops,
-             borders=borders,
-             symmetrical=TRUE,
-             rasterize=TRUE),
-  nrow = 2)
-cowplot::plot_grid(
-  plotMatrix(hic,
-             use.scores='balanced',
-             limits=c(-4,-1),
-             maxDistance=2000000,
-             caption=FALSE),
-  plotMatrix(hic,
-             use.scores='ICE',
-             limits=c(-4,-1),
-             maxDistance=2000000,
-             caption=FALSE),
-  plotMatrix(hic,
-             use.scores='expected',
-             limits=c(-4,-1),
-             maxDistance=2000000,
-             caption=FALSE),
-  plotMatrix(hic,
-             use.scores='detrended',
-             limits=c(-4,-1),
-             maxDistance=2000000,
-             caption=FALSE),
-  nrow = 2)
-plotMatrix(hic, use.scores = 'autocorrelated', limits = c(-1, 1), scale = 'linear')
-#----check resolutions here 
-hic_despeckled<-zoom(hic, 8000)
-hic_despeckled <- despeckle(hic_despeckled)
-plotMatrix(hic_despeckled, use.scores = 'balanced.despeckled', scale = 'log10', limits = c(-4, -1))
-#--this is the best plot so far
-plotMatrix(hic, use.scores = 'detrended', scale = 'linear', limits = c(-1, 1), dpi = 240)
-
 gene_interactions<-data.frame(anno)
 Gene_symbols<-gene_interactions$symbol
 q_values<-gene_interactions$qvalue
@@ -173,6 +180,7 @@ term_gene_graph(test,num_terms = c(5),use_description = TRUE)
 enrichment_chart(test, plot_by_cluster = TRUE,top_terms = 20,num_bubbles = 8)
 data<-data[order(data$highest_p), ]
 #write.csv(data,file = 'bulk.tiss_mmu_KEGG_results.csv')
+break 
 plotSaddle(hic)
 aggr_centros<-aggregate(hic,
                         targets=loops,
@@ -183,4 +191,20 @@ plotMatrix(aggr_centros,
            #scale='linear',
            cmap=bgrColors())
 break 
+#---try to get into form for use in multiHicCompare?????
+library(purrr)
+library(HiCExperiment)
+library(multiHiCcompare)
+library(tidyverse)
+BL6_cf<-CoolFile('/home/em_b/Desktop/hicool/GSE248029_il4_macro_DpnII_HinfI/mm10_output/matrices/SRR26855486_BL6.mcool')
+BL6_pairs<- PairsFile('/home/em_b/Desktop/hicool/GSE248029_il4_macro_DpnII_HinfI/mm10_output/pairs/SRR26855486_BL6.pairs')
+BL6_hic<-import(BL6_cf,
+                resolution=1000,
+                pairs=BL6_pairs,
+                format = 'mcool',
+                focus=c('chr12:24000000-28000000'))
+test<-data.frame(BL6_hic@scores@listData[["count"]])
+BL6_hic
+summary(test)
+
 
