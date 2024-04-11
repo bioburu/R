e36059bd745aaf13a32cf56ac344c6c9d430f58c
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-04-10 20:05:24 -0400

    Update BiomaRt_template.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- BiomaRt_template.R
+++ BiomaRt_template.R
@@ -1,52 +1,51 @@
 library(biomaRt)
 library(dplyr)
-setwd('/home/deviancedev/Desktop/drive_nov2023/FCCC/RNAseq_exosomes/run_2')
-matrix <- read.csv('exosomes_run2.csv')
-matrix<-cbind(matrix$Gene,matrix)
-matrix$`matrix$Gene`
-matrix$`matrix$Gene`<-sub('\\..*','',matrix$`matrix$Gene`)
-matrix$`matrix$Gene`
-#---set host to https://useast.ensembl.org or https://uswest.ensembl.org
-#---------------------------------------------------------------------
+setwd('/home/em_b/Desktop/FCCC/t3_edits')
+matrix <- read.csv('quant_hg38.csv')
+matrix<-matrix[,-c(2:3)]
 Sys.setenv("http_proxy"="http://my.proxy.org:9999")
 listMarts()
 ensembl=useMart("ensembl")
 listDatasets(ensembl)
-ensembl=useDataset("mmusculus_gene_ensembl",mart = ensembl)
+ensembl=useDataset("hsapiens_gene_ensembl",
+                   mart = ensembl)
 filters = listFilters(ensembl)
 attributes=listAttributes(ensembl)
 #-----------------------------------------------------------------
 dim(matrix)
-geneid <- matrix$`matrix$Gene`
+geneid <- matrix$Name
 head(geneid)
-dim(geneid)
 listFilters(ensembl)
 listAttributes(ensembl)
-genes <-getBM(attributes = c('ensembl_transcript_id','external_gene_name','entrezgene_id','chromosome_name','p_value','start_position','end_position','description'),
-              filters = 'ensembl_transcript_id',
+genes <-getBM(attributes = c('ensembl_transcript_id_version','external_gene_name','entrezgene_id'),
+              filters = 'ensembl_transcript_id_version',
               values = geneid,
               mart = ensembl)
 head(genes)
-df <- matrix[matrix$`matrix$Gene` %in% unique(genes$ensembl_transcript_id),]
+df <- matrix[matrix$Name %in% unique(genes$ensembl_transcript_id_version),]
 dim(df)
 dim(matrix)
-genes$ensembl_transcript_id
-colnames(genes)[1]<- 'matrix$Gene'
-Ftable <- merge(genes, df, by="matrix$Gene")
-Ftable<- Ftable[,-c(1)]
-colnames(Ftable)[7] <- c('transcript_version')
-colnames(Ftable)[1] <- c('Gene')
+genes$ensembl_transcript_id_version
+colnames(genes)[1]<- 'Gene'
+colnames(df)[1]<-'Gene'
+Ftable <- merge(genes, df, by="Gene")
+Ftable<-Ftable[order(Ftable$external_gene_name),]
+colnames(Ftable)[1] <- c('transcript_version')
+colnames(Ftable)[2] <- c('Gene')
+rownames(Ftable)<-make.names(Ftable$Gene,unique = TRUE)
+Ftable<-cbind(rownames(Ftable),Ftable)
+colnames(Ftable)[1] <- c('tracking_id')
 head(Ftable)
-matrix<-data.frame(Ftable)
+#matrix<-data.frame(Ftable)
 #------Remove duplicate genes by p_value
-matrix<-matrix%>%
-  group_by(Gene)%>%
-  arrange(p_value)%>%
-  slice(1)
-matrix<-data.frame(matrix)
-str(matrix)
+#matrix<-matrix%>%
+#  group_by(Gene)%>%
+#  arrange(p_value)%>%
+#  slice(1)
+#matrix<-data.frame(matrix)
+#str(matrix)
 break
-write.csv(matrix,file = 'exosomes_rnaseq_run2.csv')
+write.csv(Ftable,file = 'biomaRt.csv')
 #----if using older mm10 mouse assembly
 ensembl<-useEnsembl(biomart = 'genes',
                     dataset = 'mmusculus_gene_ensembl',
