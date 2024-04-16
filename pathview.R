cb3703e2195400def9d771af73b5eccc856ba62f
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-04-16 14:16:10 -0400

    Update pathview.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- pathview.R
+++ pathview.R
@@ -1,94 +1,138 @@
-library(biomaRt)
-library(dplyr)
-matrix <- read.csv('/home/deviancedev/Desktop/drive_jan2024/FCCC/exosome_exp/bulk_tissue/csv_results/bulk-tissues_quant.csv')
-matrix<-matrix[,-c(2:3)]
-#---set host to https://useast.ensembl.org or https://uswest.ensembl.org
-Sys.setenv("http_proxy"="http://my.proxy.org:9999")
-listMarts()
-ensembl=useMart("ensembl")
-listDatasets(ensembl)
-ensembl=useDataset("mmusculus_gene_ensembl",mart = ensembl)
-filters = listFilters(ensembl)
-attributes=listAttributes(ensembl)
-#-----------------------------------------------------------------
-dim(matrix)
-geneid <- matrix$Gene
-head(geneid)
-dim(geneid)
-listFilters(ensembl)
-listAttributes(ensembl)
-genes <-getBM(attributes = c('ensembl_transcript_id_version','entrezgene_id','external_gene_name','chromosome_name','p_value','start_position','end_position','description'),
-              filters = 'ensembl_transcript_id_version',
-              values = geneid,
-              mart = ensembl)
-head(genes)
-df <- matrix[matrix$Gene %in% unique(genes$ensembl_transcript_id_version),]
-dim(df)
-dim(matrix)
-colnames(genes)[1]<- 'Gene'
-Ftable <- merge(genes, df, by="Gene")
-colnames(Ftable)[1] <- c('transcript_version')
-colnames(Ftable)[3] <- c('Gene')
-Ftable<-Ftable[!duplicated(Ftable$transcript_version),]
-Ftable<-data.frame(Ftable)
-row.names(Ftable)<-make.names(Ftable$Gene,unique = TRUE)
-str(Ftable)
-Ftable<-cbind(row.names(Ftable),Ftable)
-colnames(Ftable)[4]<-'external_gene_name'
-colnames(Ftable)[1]<-'R_trackingID'
-break
-setwd('/home/deviancedev/Desktop/drive_jan2024/FCCC/exosome_exp/bulk_tissue/csv_results')
-#write.csv(Ftable,file = 'bulk_tissues_biomart.csv')
-library(DESeq2)
-library(pheatmap)
-library(dplyr)
-matrix<-Ftable[,-c(1:9)]
-colnames(matrix)<-c('Norm_1','Norm_2','Norm_3','MB_1','MB_2','MB_3')
-matrix<-round(matrix)
-head(matrix)
-names<-colnames(matrix)
-condition<-c('A','A','A','B','B','B')
-type<-c('paired','paired','paired','paired','paired','paired')
-coldata<-data.frame(cbind(names,condition,type))
-row.names(coldata)<-coldata$names
-coldata<-coldata[,-1]
-head(coldata)
-#-----------------------------------------------------------------------------
-deseq<-DESeqDataSetFromMatrix(countData = matrix,colData = coldata,design = ~ condition)
-deseq
-DE<-DESeq(deseq)
-plotMA(DE,ylim=c(-2,2))
-plotDispEsts(DE)
-#-------------------------------------------------------------------------------
-results<-results(DE)
-results
-x<-data.frame(results)
-summary(x)
-x<-na.omit(x)
-#----only upregulated genes 
-downreg<-subset(x,log2FoldChange< -1.5)
-upreg<-subset(x,log2FoldChange> 1.5)
-df<-rbind(upreg,downreg)
-
-df<-cbind(row.names(df),df)
-df$`row.names(df)`<-sub('\\..*','',df$`row.names(df)`)
-colnames(df)[1]<-'external_gene_name'
-df<-cbind(row.names(df),df)
-colnames(df)[1]<-'R_trackingID'
-biomart_output<-read.csv('/home/deviancedev/Desktop/drive_jan2024/FCCC/exosome_exp/bulk_tissue/csv_results/bulk_tissues_biomart.csv',row.names = 1)
-df <- merge(df, biomart_output,by="R_trackingID")
-df<-df[,-11]
-colnames(df)[2]<-'external_gene_name'
-df<-df[order(df$padj, decreasing=FALSE),]
-df<-subset(df,padj< 0.05)
-df<-df[,-12]
-setwd('/home/deviancedev/Desktop/drive_jan2024/FCCC/exosome_exp/bulk_tissue/csv_results')
-#write.csv(df,file = 'bulk_tissues_deseq2.csv')
-#--------------
+library(GenomicRanges)
+library(TxDb.Mmusculus.UCSC.mm39.refGene)
+library(TxDb.Mmusculus.UCSC.mm39.knownGene)
+library(ChIPseeker)
+library(clusterProfiler)
+library(org.Mm.eg.db)
+library(enrichplot)
+library(ReactomePA)
+library(plyranges)
+library(BRGenomics)
+library(ggplot2)
+library(UpSetR)
+library(ChIPpeakAnno)
 library(pathview)
-foldchanges = df$log2FoldChange
-names(foldchanges) = df$entrezgene_id
-head(foldchanges)
-pv.out <- pathview(gene.data = foldchanges, pathway.id = "05022",
-                   species = "mmu", out.suffix = "mmu_test")
+setwd('/home/em_b/work_stuff/FCCC/chipseq/comb/macs/D1/plots')
+txdb<-TxDb.Mmusculus.UCSC.mm39.refGene
+peak <- readPeakFile('/home/em_b/work_stuff/FCCC/chipseq/comb/macs/D1/D1_A1_peaks.narrowPeak')
+table(seqnames(peak))
+peak
+peak<-tidyChromosomes(peak,
+                keep.X=FALSE,
+                keep.Y=FALSE,
+                keep.M = FALSE,
+                keep.nonstandard = FALSE,
+                genome = 'mm39')
+table(seqnames(peak))
+covplot(peak, weightCol="V5")
+summary(peak$V5)
+#covplot(peak, weightCol="V5", chrs=c("chr1", "chr19"))
+gc()
+plotAvgProf2(peak, TxDb=txdb, upstream=3000, downstream=3000,
+             xlab="Genomic Region (5'->3')", ylab = "Read Count Frequency",
+             conf = 0.95)
+plotPeakProf2(peak = peak, upstream = 3000, downstream = 3000,
+              conf = 0.95, by = "gene", type = "body", nbin = 800,
+              TxDb = txdb, weightCol = "V5",ignore_strand = F)
+gc()
+macsPeaks <- '/home/em_b/work_stuff/FCCC/chipseq/comb/macs/D1/D1_A1_peaks.xls'
+htmapdf <- read.delim(macsPeaks,comment.char="#")
+summary(htmapdf$X.log10.qvalue.)
+#df<-subset(df,X.log10.qvalue.>84)
+htmapdf<-GRanges(htmapdf)
+htmapdf<-tidyChromosomes(htmapdf,
+                    keep.X=FALSE,
+                    keep.Y=FALSE,
+                    keep.M = FALSE,
+                    keep.nonstandard = FALSE)
+table(htmapdf@seqnames)
+#peakHeatmap(htmapdf,
+#            TxDb = TxDb.Mmusculus.UCSC.mm39.knownGene,
+#            nbin = 800,
+#            upstream=3000,
+#            downstream=3000)
+peakHeatmap(peak = htmapdf,
+            TxDb = txdb,
+            upstream = rel(0.2),
+            downstream = rel(0.2),
+            by = "gene",
+            type = "body",
+            nbin = 800)+
+  scale_fill_distiller(palette = 'Greys')
+df <- read.delim(macsPeaks,comment.char="#")
+table(df$chr)
+list<-c('chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10',
+        'chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19')
+macsPeaks_DF<-subset(df, subset = chr %in% list)
+table(macsPeaks_DF$chr)
+macsPeaks_GR <- GRanges(seqnames=macsPeaks_DF[,"chr"],
+                        IRanges(macsPeaks_DF[,"start"],macsPeaks_DF[,"end"]))
+mcols(macsPeaks_GR) <- macsPeaks_DF[,c("abs_summit", "fold_enrichment")]
+macsPeaks_GR[1:5,]
+peakAnno <- annotatePeak(macsPeaks_GR, tssRegion=c(-1000, 1000), 
+                         TxDb=txdb, 
+                         annoDb="org.Mm.eg.db")
+peakAnno
+plotAnnoPie(peakAnno)
+#plotAnnoBar(peakAnno)
+vennpie(peakAnno)
+res <- genomicElementUpSetR(peak,
+                            TxDb.Mmusculus.UCSC.mm39.knownGene)
+upset(res[["plotData"]], 
+      nsets = length(colnames(res$plotData)), 
+      nintersects = NA)
+plotDistToTSS(peakAnno,
+              title="Distribution of transcription factor-binding loci\nrelative to TSS")
+#------Reactome pathway 
+cat('Reactome is for all peaks general output')
+gene <- seq2gene(peak, tssRegion = c(-1000, 1000), flankDistance = 3000, TxDb=txdb)
+gene
+reactome <- enrichPathway(gene,
+                          organism = 'mouse')
+dotplot(reactome)
+reactome<-data.frame(reactome)
+#write.csv(pathway,file='reactome_pathway.csv')
+#-------------------------------------------------------------------------------
+cat('KEGG is for all peaks general output')
+kegg<-data.frame(peakAnno)
+#kegg <- kegg[kegg$annotation == "Promoter",]
+kegg_genes<-kegg$fold_enrichment
+names(kegg_genes)<-kegg$geneId
+kegg_genes
+pathview(gene.data = kegg_genes,
+               pathway.id = '04919',
+               species = 'mmu',
+               out.suffix='mmu_test')
+pathview(gene.data = kegg,
+               pathway.id = '04722',
+               species = 'mmu',
+               out.suffix='mmu_test')
+#-------------------------------------------------------------------------------
+annotatedPeaksGR <- as.GRanges(peakAnno)
+annotatedPeaksDF <- as.data.frame(peakAnno)
+annotatedPeaksDF
+annotatedPeaksGR$annotation
+annotatedPeaksGR_TSS <- annotatedPeaksGR[annotatedPeaksGR$annotation == "Promoter",]
+annotatedPeaksGR_TSS
+#duplicated(annotatedPeaksGR_TSS$geneId)
+genesWithPeakInTSS <- unique(annotatedPeaksGR_TSS$geneId)
+genesWithPeakInTSS
+allGeneGR <- genes(TxDb.Mmusculus.UCSC.mm39.refGene)
+allGeneGR[1:2, ]
+#-----Gene ontology enrichment
+genesWithPeakInTSS
+cat('GO is refined to only the selected regions (promoter,distal-intergenic)')
+GO_result <- enrichGO(gene = genesWithPeakInTSS,
+                      #universe = allGeneIDs,
+                      OrgDb = org.Mm.eg.db,
+                      ont = "BP")
+GO_result
+GO_result_df <- data.frame(GO_result)
+GO_result_df[1:2, ]
+GO_result_df<-GO_result_df[order(GO_result_df$Count, decreasing=TRUE),]
+GO_result_plot <- pairwise_termsim(GO_result)
+emapplot(GO_result_plot, showCategory = 50)
+write.csv(annotatedPeaksDF,file = 'all_annotated_peaks.csv')
+write.csv(reactome,file = 'reactome.csv')
+write.csv(GO_result_df,file = 'GO.csv')
 break 
