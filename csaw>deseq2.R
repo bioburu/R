3f09d9fa2ae5b90285a4cc250b0e16d6cae50e09
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-04-25 12:56:38 -0400

    Update csaw>deseq2.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- csaw>deseq2.R
+++ csaw>deseq2.R
@@ -6,20 +6,22 @@ library(TxDb.Mmusculus.UCSC.mm39.refGene)
 library(clusterProfiler)
 library(enrichplot)
 library(pathview)
+setwd('/home/em_b/work_stuff/FCCC/chipseq')
 txdb<-TxDb.Mmusculus.UCSC.mm39.refGene
 #-----minq= minimum quality score
 param <- readParam(minq=20)
 #----------generate counts for bins in all files
-bam.files<-c('/home/em_b/work_stuff/FCCC/chipseq/comb/bam/G2.B22.Me3.rmdup.sort.bam',
-             '/home/em_b/work_stuff/FCCC/chipseq/comb/bam/E2.B22.Me3.rmdup.sort.bam',
-             '/home/em_b/work_stuff/FCCC/chipseq/comb/bam/G3.T3.Me3.rmdup.sort.bam',
-             '/home/em_b/work_stuff/FCCC/chipseq/comb/bam/E3.T3.me3.rmdup.sort.bam')
+bam.files<-c(THRA_0hr='/home/em_b/work_stuff/FCCC/chipseq/bam/D1.0h.tra.rmdup.sort.bam',
+             THRA_0hr='/home/em_b/work_stuff/FCCC/chipseq/bam/F1.0h.Tra.rmdup.sort.bam',
+             THRA_0hr='/home/em_b/work_stuff/FCCC/chipseq/bam/H1.0h.Tra.rmdup.sort.bam',
+             THRA_t3='/home/em_b/work_stuff/FCCC/chipseq/bam/D3.t3.tra.rmdup.sort.bam',
+             THRA_t3='/home/em_b/work_stuff/FCCC/chipseq/bam/F3.T3.Tra.rmdup.sort.bam',
+             THRA_t3='/home/em_b/work_stuff/FCCC/chipseq/bam/H3.T3_tra.rmdup.sort.bam')
+bam.files
 #-----count read overlaps in windows. ext= average length of fragments. width= width of window. 
 #data <- windowCounts(bam.files, ext=850, width=50, param=param,bin = TRUE)
 data <- windowCounts(bam.files, ext=500, width=50, param=param,bin = TRUE)
 data
-colnames(data)<-c('G2.B22.Me3','E2.B22.Me3','G3.T3.Me3','E3.T3.me3')
-
 #----get larger bins for normalization 
 binned <- windowCounts(bam.files, bin=TRUE, width=10000, param=param)
 binned
@@ -29,7 +31,7 @@ data$totals
 #---------filter out uninformative regions ie low counts
 #-- the higher the filter value, the more restrictive 
 keep <- filterWindowsGlobal(data=data,
-                            background=binned)$filter > log2(5)
+                            background=binned)$filter > 5
 summary(keep)
 dim(data)
 data <- data[keep,]
@@ -43,14 +45,13 @@ ranges<-data.frame(data@rowRanges)
 matrix<-cbind(ranges,counts)
 head(matrix)
 summary(matrix)
-rm(binned,counts,data,param,ranges,x)
 gc()
 counts_in<-matrix
 matrix<-matrix[,-c(1:5)]
 names<-colnames(matrix)
 names
-condition<-c('A','A','B','B')
-type<-c('paired','paired','paired','paired')
+condition<-c('A','A','A','B','B','B')
+type<-c('paired','paired','paired','paired','paired','paired')
 coldata<-data.frame(cbind(names,condition,type))
 row.names(coldata)<-coldata$names
 coldata<-coldata[,-1]
@@ -58,41 +59,25 @@ head(coldata)
 #-----------------------------------------------------------------------------
 deseq<-DESeqDataSetFromMatrix(countData = matrix,colData = coldata,design = ~ condition)
 deseq
-
 deseq<-estimateSizeFactors(deseq)
-
 deseq<-estimateDispersions(deseq)
-
 test <- DESeq(deseq, test="LRT", reduced= ~ 1)
-
-
-test<-nbinomLRT(deseq,
-                reduced = 1)
-
+test<-nbinomLRT(deseq,reduced = 1)
 resLRT <- results(test)
-
 resLRT<-data.frame(resLRT)
-
+resLRT
 df<-cbind(counts_in,resLRT)
-
 df<-subset(df, pvalue<0.05)
-
 list<-c('chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10',
         'chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19')
 DF<-subset(df, subset = seqnames %in% list)
-
 summary(DF$seqnames)
-
-
 DF<-GRanges(DF)
-
-peakAnno <- annotatePeak(DF, tssRegion=c(-1000, 1000), 
-                        TxDb=txdb, 
-                        annoDb="org.Mm.eg.db")
+peakAnno <- annotatePeak(DF,
+                         TxDb=txdb, 
+                         annoDb="org.Mm.eg.db")
 out<-data.frame(peakAnno)
-
 out
-setwd('/home/em_b/work_stuff/FCCC/chipseq')
 #write.csv(out,file = 'initial_deg_chipseq.csv')
 #-------------------------
 gr_out<-GRanges(out)
