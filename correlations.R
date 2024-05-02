ecb4ec2b09cbd4b558a42d43f9699ff336abba81
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-05-02 11:55:12 -0400

    Update correlations.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- correlations.R
+++ correlations.R
@@ -2,10 +2,21 @@
 FeatureScatter(data, feature1 = "Foxo1", feature2 = 'Dio2',cols = c('skyblue','red','grey','black'),pt.size = 5,
                shuffle = TRUE,seed = 123)
 #---for data frame with variables as colnames (transposed)
-ggscatter(df,x='Foxo1',y='Dio2',
-          add='reg.line',conf.int = TRUE,
-          cor.coef = TRUE,cor.method = 'spearman',
-          xlab='Foxo1',ylab = 'Dio2',shape = 21,size=2)
+ggscatter(boxplot,
+          x='NEUROD1',y='EZH2',
+          add='reg.line',
+          conf.int = TRUE,
+          cor.coef = TRUE,
+          cor.method = 'spearman',
+          xlab='NEUROD1',
+          ylab = 'EZH2',
+          shape = 24,
+          size=4,
+          cor.coef.size = 4,
+          ggtheme = theme_minimal(),
+          label='classes',
+          font.label = c(10,'bold.italic','blue'),
+          add.params = list(color='red',fill='lightgrey'))
 
 #--------------for scRNAseq
 df<-FetchData(data,vars = c('ident',top10000),layer = 'counts')
