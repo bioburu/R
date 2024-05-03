6d3839212c04296f8f19dbfb1d500328746c3f37
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-05-03 10:16:23 -0400

    Update deseq2_by_pathways.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- deseq2_by_pathways.R
+++ deseq2_by_pathways.R
@@ -6,23 +6,24 @@ library(org.Mm.eg.db)
 library(biomaRt)
 library(dplyr)
 library(RColorBrewer)
-setwd('/home/em_b/work_stuff/FCCC/T3_RNAseq')
-matrix<-read.csv('biomart.mm39.csv')
-go_neural_diff<-read.delim('/home/em_b/Downloads/GO_term_summary_20240502_214014.txt',
+setwd('/home/em_b/work_stuff/FCCC/T3_RNAseq/R')
+cat('GO list taken from https://www.informatics.jax.org/go/term/GO:0030182')
+matrix<-read.csv('/home/em_b/work_stuff/FCCC/T3_RNAseq/biomart.mm39.csv')
+go<-read.delim('/home/em_b/work_stuff/FCCC/T3_RNAseq/R/gene_ontology_list/GO:0030182_mmus_neuron_differentiation.txt',
                  row.names = NULL)
-list<-go_neural_diff$MGI.Gene.Marker.ID
+colnames(go)
+list<-go$MGI.Gene.Marker.ID
 list
-colnames(go_neural_diff)
+colnames(matrix)
+dim(matrix)
 matrix<-subset(matrix, subset = Gene %in% list)
-str(matrix)
+dim(matrix)
 row.names(matrix)<-make.names(matrix$Gene,unique = TRUE)
 matrix<-matrix[,-1]
 head(matrix)
-tester<-matrix[,-c(7:27)]
-colnames(tester)
-colnames(tester)<-c('PBS','PBS.1','PBS.2',
-                    'T3','T3.1','T3.2')
-names<-colnames(tester)
+mb.pbs.t3<-matrix[,-c(7:27)]
+colnames(mb.pbs.t3)
+names<-colnames(mb.pbs.t3)
 names
 condition<-c('A','A','A','B','B','B')
 type<-c('paired','paired','paired','paired','paired','paired')
@@ -32,10 +33,10 @@ row.names(coldata)<-make.names(coldata$names,
 coldata<-coldata[,-1]
 coldata
 #-----------------------------------------------------------------------------
-tester<-round(tester)
-colnames(tester)
+mb.pbs.t3<-round(mb.pbs.t3)
+colnames(mb.pbs.t3)
 row.names(coldata)
-deseq<-DESeqDataSetFromMatrix(countData = tester,colData = coldata,design = ~ condition)
+deseq<-DESeqDataSetFromMatrix(countData = mb.pbs.t3,colData = coldata,design = ~ condition)
 deseq
 #----if using standard negbinomial walds test  
 DE<-DESeq(deseq)
@@ -45,21 +46,29 @@ plotDispEsts(DE)
 results<-results(DE)
 results
 x<-data.frame(results)
-x<-x[order(x$pvalue, decreasing=FALSE),]
-x<-subset(x,pvalue< 0.05)
-x<-subset(x,log2FoldChange> 0)
-x
-x
-names<-row.names(x)
-str(names)
-names<-append(names, 'Neurod1', after = 0)
-pheatmap(tester[names,],
+htmp<-x[order(x$pvalue, decreasing=FALSE),]
+htmp<-subset(htmp,pvalue< 0.09)
+htmp<-subset(htmp,log2FoldChange> 0)
+head(htmp)
+go_genes<-row.names(htmp)
+str(go_genes)
+go_genes<-append(go_genes, 'Neurod1', after = 0)
+go_genes
+colnames(mb.pbs.t3)<-c('PBS','PBS','PBS',
+                    'T3','T3','T3')
+pheatmap(mb.pbs.t3[go_genes,],
          scale = 'row',
          cluster_cols = FALSE,
          cluster_rows = FALSE,
          border_color = 'grey',
-         fontsize = 17,
+         fontsize = 9,
          color = colorRampPalette(rev(brewer.pal(n=7,name='RdYlBu')))(100))
+nd1<-subset(x,subset=row.names(x) %in% 'Neurod1')
+nd1
+final<-rbind(nd1,htmp)
+final<-final[,-c(1,3,4,6)]
+final
+write.csv(final,file='R1Q5_neural_differentiation_genes.csv')
 break 
 #----if using LRT---------------------------------------------- 
 deseq<-estimateSizeFactors(deseq)
