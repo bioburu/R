5831d6ce531995ca4fa33551fdba82ea088d1c83
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-05-14 11:19:01 -0400

    Update diffbind.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- diffbind.R
+++ diffbind.R
@@ -1,16 +1,19 @@
+#----MUST BE ALIGNED TO MM10 OR HG38 
 library(DiffBind)
-samples<-read.csv('/home/em_b/work_stuff/ChIP_RNA_comparison_templates/diffbind_test_samplesheet.csv')
+library(GenomicRanges)
+samples<-read.csv('/home/em_b/Desktop/diffbind_metadata.csv')
 samples
-#dbObj<-dba(sampleSheet = samples)
-#dbObj
-#plot(dbObj)
-test<-dba.analyze(samples)#,
-                  #method = DBA_ALL_METHODS,
-                  #bBlacklist = TRUE,
-                  #bGreylist = TRUE,
+dbObj<-dba(sampleSheet = samples)
+dbObj
+plot(dbObj)
+Samples<-GRanges(samples)
+test<-dba.analyze(samples,
+                  method = DBA_ALL_METHODS,
+                  bBlacklist = DBA_BLACKLIST_MM10,
+                  bGreylist = DBA_BLACKLIST_MM10,
                   #bRetrieveAnalysis = TRUE,
-                  #bParallel = TRUE
-                  #)
+                  bParallel = TRUE
+                  )
 cat('ERROR-----Unable to complete analysis.
 Warning messages:
 1: No contrasts added. There must be at least two sample groups with at least three replicates. 
