7e35ff4342531ee241ad7f6b8a18bb7181c6c437
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-05-20 23:19:10 -0400

    Update gviz.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- gviz.R
+++ gviz.R
@@ -11,7 +11,7 @@ chr<-'chr9'
 bm <- useEnsembl(biomart = "ENSEMBL_MART_ENSEMBL", 
                  dataset = "mmusculus_gene_ensembl")
 biomTrack <- BiomartGeneRegionTrack(genome = "mm39",
-                                    #chromosome = 9, 
+                                    #chromosome = chr, 
                                     #start = 79282981,
                                     #end = 79286980,
                                     name = "ENSEMBL",
@@ -30,43 +30,19 @@ cat('add bases to genomic tracks')
 strack <- SequenceTrack(Mmusculus, chromosome = chr)
 plotTracks(list(gtrack,itrack,strack,biomTrack),
            transcriptAnnotation='symbol')
-#-----DataTrack constructor is important
-set.seed(255)
-lim <- c(75213570, 75345923)
-lim
-lim[1]
-lim[2]
-cat('These are randomly sample coordinates')
-coords <- sort(c(lim[1], 
-                 sample(seq(from = lim[1], to = lim[2]), 99), 
-                 lim[2]))
-coords
-dat <- runif(100, min = -10, max = 10)
-dat
-cat('look hard at DataTrack constructor')
-coords[-length(coords)]
-coords[-1]
-dtrack <- DataTrack(data = dat, start = coords[-length(coords)],
-                    end = coords[-1], chromosome = chr, genome = gen, 
-                    name = "Uniform")
-Test<-read.delim('/home/em_b/work_stuff/chipseq2/thra_0hr_comb/peaks/thra_0hr_IgG_peaks.narrowPeak',
+chip<-read.delim('/home/em_b/work_stuff/chipseq2/thra_0hr_comb/peaks/thra_0hr_IgG_peaks.narrowPeak',
                  header = FALSE)
-head(Test)
-colnames(Test)<-c('chr','start','end','peak','score','na','FC','pval','qval','summit')
-head(Test)
+head(chip)
+colnames(chip)<-c('chr','start','end','peak','score','na','FC','pval','qval','summit')
+head(chip)
 list<-c('chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10',
         'chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19')
-Test<-subset(Test, subset = chr %in% list)
-table(Test$chr)
-#thra_0hr<-GRanges(Test)
-#thra_0hr<-DataTrack(thra_0hr,
-#                    name = 'thra_0hr')
-
-test<-DataTrack(data = Test$FC, start = Test$start,
-                end = Test$end, chromosome = chr, genome = gen, 
-                name = "Test")
-cat('options for types are: histogram,horizon,heatmap,polygon,mountain,b and you can layer')
-plotTracks(list(gtrack,itrack,strack,biomTrack,dtrack,test),
+chip<-subset(chip, subset = chr %in% list)
+table(chip$chr)
+chip<-DataTrack(data = chip$FC, start = chip$start,
+                end = chip$end, chromosome = chr, genome = gen, 
+                name = 'Chip-seq')
+plotTracks(list(gtrack,itrack,strack,biomTrack,chip),
            chromosome = chr,
            transcriptAnnotation='symbol',
            extend.left = 0.5,
@@ -76,5 +52,25 @@ plotTracks(list(gtrack,itrack,strack,biomTrack,dtrack,test),
            to=75545923,
            type=c('histogram'),
            legend=TRUE)
-summary(Test$FC)
-break 
+gc()
+rna <- DataTrack(range = '/home/em_b/work_stuff/chipseq2/rna/SRR23386662/SRR23386662.sorted.bam',
+                     genome = "mm39",
+                     type = "h", 
+                     name = 'RNAseq',
+                     #window = -1, 
+                     chromosome = 'chr9')
+class(rna)
+rna
+cat('options for types are: histogram,horizon,heatmap,polygon,mountain,b and you can layer')
+
+plotTracks(list(gtrack,itrack,strack,biomTrack,chip,rna),
+           chromosome = chr,
+           transcriptAnnotation='symbol',
+           extend.left = 0.5,
+           extend.right = 100000,
+           col=c('skyblue'),
+           from = 75013570,
+           to=75545923,
+           type=c('b'),
+           legend=TRUE)
+
