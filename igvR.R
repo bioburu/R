768f9bd394489b9ccf3e925f156f5d5f3dd242c0
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-06-10 14:05:58 -0400

    Update igvR.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- igvR.R
+++ igvR.R
@@ -2,33 +2,22 @@ library(igvR)
 igv<-igvR()
 setBrowserWindowTitle(igv, "ChIP-seq")
 setGenome(igv, 'mm39')
-#showGenomicRegion(igv, "chr3:128,079,020-128,331,275")
-# or
-showGenomicRegion(igv, 'NEUROD1')
-for(i in 1:4) zoomOut(igv)
-roi <- getGenomicRegion(igv)
-gr.roi <- with(roi, GRanges(seqnames=chrom, ranges = IRanges(start, end)))
-param <- ScanBamParam(which=gr.roi, what = scanBamWhat())
-#---enter in bam file
-bamFile <- '/home/em_b/work_stuff/chipseq2/thra_0hr_comb/thra_0hr_comb.bam'
+showGenomicRegion(igv, 'Cxcl5')
+#for(i in 1:2) zoomOut(igv)
+bamFile <- '/home/em_b/Desktop/chip_validations_final/bam/D1.0h.tra.rmdup.sort.bam'
 alignments <- readGAlignments(bamFile, use.names=TRUE, param=param)
-track <- GenomicAlignmentTrack(trackName='thra_0hr', alignments, visibilityWindow=10000000, trackHeight=200) 
-track
+track <- GenomicAlignmentTrack(trackName='D1', alignments, visibilityWindow=10000000, trackHeight=200) 
 displayTrack(igv, track)
-#----enter in peaks-------------------------
-tbl.pk <- read.delim('/home/em_b/work_stuff/chipseq2/thra_0hr_comb/peaks/thra_0hr_3rep_peaks.narrowPeak',
-                     header=FALSE)
-dim(tbl.pk) 
-colnames(tbl.pk)<-c('chrom','start','end','name','score')
-head(tbl.pk)
-tbl.pk<-tbl.pk[,-c(4,6:10)]
-unlist(lapply(tbl.pk, class))
-track <- DataFrameQuantitativeTrack("macs3_peaks", tbl.pk, color="red", autoscale=TRUE)
-displayTrack(igv, track)
-#-------enter in bam file 
-bamFile <- '/home/em_b/work_stuff/chipseq2/thra_0hr_comb/thra_0hr_comb_input.bam'
+bamFile <- '/home/em_b/Desktop/chip_validations_final/bam/A1.0hr.input.rmdup.sort.bam'
 alignments <- readGAlignments(bamFile, use.names=TRUE, param=param)
-track <- GenomicAlignmentTrack(trackName='thra_0hr_input', alignments, visibilityWindow=10000000, trackHeight=200) 
-track
+track2 <- GenomicAlignmentTrack(trackName='input', alignments, visibilityWindow=10000000, trackHeight=200) 
+displayTrack(igv,track2)
+peak <- read.delim('/home/em_b/Desktop/chip_validations_final/in_house/peaks_q0.01/A1_D1_peaks.xls',
+                     comment.char="#")
+head(peak)
+peak_df<-data.frame(chrom=peak$chr,
+                   start=peak$start,
+                   end=peak$end,
+                   score=peak$pileup)
+track <- DataFrameQuantitativeTrack("macs3_peaks", peak_df, color="red", autoscale=TRUE)
 displayTrack(igv, track)
-break 
