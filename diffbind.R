e97daf66a502fa67b63d1eb351765c6311eb1eed
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-06-11 16:21:46 -0400

    Update diffbind.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- diffbind.R
+++ diffbind.R
@@ -12,24 +12,9 @@ mm39<-query_data[['AH107321']]
 mm39
 #-----------------------------
 
-
-#----MUST BE ALIGNED TO MM10 OR HG38 
 library(DiffBind)
-library(GenomicRanges)
-samples<-read.csv('/home/em_b/Desktop/diffbind_metadata.csv')
-samples
-dbObj<-dba(sampleSheet = samples)
-dbObj
-plot(dbObj)
-Samples<-GRanges(samples)
-test<-dba.analyze(samples,
-                  method = DBA_ALL_METHODS,
-                  bBlacklist = DBA_BLACKLIST_MM10,
-                  bGreylist = DBA_BLACKLIST_MM10,
-                  #bRetrieveAnalysis = TRUE,
-                  bParallel = TRUE
-                  )
-cat('ERROR-----Unable to complete analysis.
-Warning messages:
-1: No contrasts added. There must be at least two sample groups with at least three replicates. 
-2: No contrasts added. There must be at least two sample groups with at least three replicates. ')
+db<-dba(sampleSheet='/home/em_b/Desktop/diffbind/diffbind_meta.csv')
+plot(db)
+counts<-dba.count(db,
+                  summits=250,
+                  bParallel =FALSE)
