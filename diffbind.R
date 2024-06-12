1565cdc54fe323c1abab712848e62d5c319b689f
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-06-12 15:43:37 -0400

    Update diffbind.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- diffbind.R
+++ diffbind.R
@@ -14,7 +14,21 @@ mm39
 
 library(DiffBind)
 db<-dba(sampleSheet='/home/em_b/Desktop/diffbind/diffbind_meta.csv')
+db
 plot(db)
 counts<-dba.count(db,
-                  summits=250,
+                  #filter = 0,
+                  #summits =FALSE, 
                   bParallel =FALSE)
+counts
+plot(counts)
+counts_norm<-dba.normalize(counts)
+counts_norm$norm
+counts_norm<-dba.contrast(counts_norm,
+                          contrast = c('Treatment','none','treated'))
+counts_norm$contrasts
+counts_norm<-dba.analyze(counts_norm)
+dba.show(counts_norm,bContrasts = TRUE)
+plot(counts_norm,contrast=1)
+dba.report(counts_norm)
+counts_norm[["contrasts"]][[1]][["DESeq2"]][["de"]]
