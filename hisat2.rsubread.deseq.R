2a96666f45a12006789d9182d68bacd0c517cfd5
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-06-25 13:30:29 -0400

    Update hisat2.rsubread.deseq.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- hisat2.rsubread.deseq.R
+++ hisat2.rsubread.deseq.R
@@ -2,61 +2,62 @@ library(Rsubread)
 library(biomaRt)
 library(dplyr)
 library(DESeq2)
-counts<-featureCounts(files = '/home/em_b/work_stuff/chipseq2/rna/SRR23386662/SRR23386662.rmdup.sort.chr.bam',
+library(EnhancedVolcano)
+counts<-featureCounts(files = '/home/em_b/Desktop/dio2_il6/bam/GA1_norm_ast.rmdup.sort.chr1.19.bam',
                       annot.inbuilt = 'mm39',
                       isPairedEnd = TRUE)
-T3_1<-data.frame(counts$counts)
-T3_1<-cbind(row.names(T3_1),T3_1)
-colnames(T3_1)<-c('Gene','MB.T3_1')
-head(T3_1)
+norm_1<-data.frame(counts$counts)
+norm_1<-cbind(row.names(norm_1),norm_1)
+colnames(norm_1)<-c('Gene','norm_1')
+head(norm_1)
 #-------------------------------------------------------------------------------
-counts<-featureCounts(files = '/home/em_b/work_stuff/chipseq2/rna/SRR23386663/SRR23386663.rmdup.sort.chr.bam',
+counts<-featureCounts(files = '/home/em_b/Desktop/dio2_il6/bam/GA2_norm_ast.rmdup.sort.chr1.19.bam',
                       annot.inbuilt = 'mm39',
                       isPairedEnd = TRUE)
-T3_2<-data.frame(counts$counts)
-T3_2<-cbind(row.names(T3_2),T3_2)
-colnames(T3_2)<-c('Gene','MB.T3_2')
-head(T3_2)
+norm_2<-data.frame(counts$counts)
+norm_2<-cbind(row.names(norm_2),norm_2)
+colnames(norm_2)<-c('Gene','norm_2')
+head(norm_2)
 #-------------------------------------------------------------------------------
-counts<-featureCounts(files = '/home/em_b/work_stuff/chipseq2/rna/SRR23386664/SRR23386664.rmdup.sort.chr.bam',
+counts<-featureCounts(files = '/home/em_b/Desktop/dio2_il6/bam/GA3_norm_ast.rmdup.sort.chr1.19.bam',
                       annot.inbuilt = 'mm39',
                       isPairedEnd = TRUE)
-T3_3<-data.frame(counts$counts)
-T3_3<-cbind(row.names(T3_3),T3_3)
-colnames(T3_3)<-c('Gene','MB.T3_3')
-head(T3_3)
+norm_3<-data.frame(counts$counts)
+norm_3<-cbind(row.names(norm_3),norm_3)
+colnames(norm_3)<-c('Gene','norm_3')
+head(norm_3)
 #-=-----------------------------------------------------------------------------
-counts<-featureCounts(files = '/home/em_b/work_stuff/chipseq2/rna/SRR23386665/SRR23386665.rmdup.sort.chr.bam',
+counts<-featureCounts(files = '/home/em_b/Desktop/dio2_il6/bam/AS1_tumor_ast.rmdup.sort.chr1.19.bam',
                       annot.inbuilt = 'mm39',
                       isPairedEnd = TRUE)
-PBS_1<-data.frame(counts$counts)
-PBS_1<-cbind(row.names(PBS_1),PBS_1)
-colnames(PBS_1)<-c('Gene','MB.PBS_1')
-head(PBS_1)
+tumor_1<-data.frame(counts$counts)
+tumor_1<-cbind(row.names(tumor_1),tumor_1)
+colnames(tumor_1)<-c('Gene','tumor_1')
+head(tumor_1)
 #-------------------------------------------------------------------------------
-counts<-featureCounts(files = '/home/em_b/work_stuff/chipseq2/rna/SRR23386666/SRR23386666.rmdup.sort.chr.bam',
+counts<-featureCounts(files = '/home/em_b/Desktop/dio2_il6/bam/AS4_tumor_ast.rmdup.sort.chr1.19.bam',
                       annot.inbuilt = 'mm39',
                       isPairedEnd = TRUE)
-PBS_2<-data.frame(counts$counts)
-PBS_2<-cbind(row.names(PBS_2),PBS_2)
-colnames(PBS_2)<-c('Gene','MB.PBS_2')
-head(PBS_2)
+tumor_2<-data.frame(counts$counts)
+tumor_2<-cbind(row.names(tumor_2),tumor_2)
+colnames(tumor_2)<-c('Gene','tumor_2')
+head(tumor_2)
 #-------------------------------------------------------------------------------
-counts<-featureCounts(files = '/home/em_b/work_stuff/chipseq2/rna/SRR23386667/SRR23386667.rmdup.sort.chr.bam',
+counts<-featureCounts(files = '/home/em_b/Desktop/dio2_il6/bam/AS5_tumor_ast.rmdup.sort.chr1.19.bam',
                       annot.inbuilt = 'mm39',
                       isPairedEnd = TRUE)
-PBS_3<-data.frame(counts$counts)
-PBS_3<-cbind(row.names(PBS_3),PBS_3)
-colnames(PBS_3)<-c('Gene','MB.PBS_3')
-head(PBS_3)
+tumor_3<-data.frame(counts$counts)
+tumor_3<-cbind(row.names(tumor_3),tumor_3)
+colnames(tumor_3)<-c('Gene','tumor_3')
+head(tumor_3)
 #-------------------------------------------------------------------------------
-matrix<-cbind(PBS_1,
-              PBS_2$MB.PBS_2,
-              PBS_3$MB.PBS_3,
-              T3_1$MB.T3_1,
-              T3_2$MB.T3_2,
-              T3_3$MB.T3_3)
-colnames(matrix)<-c('Gene','PBS_1','PBS_2','PBS_3','T3_1','T3_2','T3_3')
+matrix<-cbind(norm_1,
+              norm_2$norm_2,
+              norm_3$norm_3,
+              tumor_1$tumor_1,
+              tumor_2$tumor_2,
+              tumor_3$tumor_3)
+colnames(matrix)<-c('Gene','norm.ast_1','norm.ast_2','norm.ast_3','tumor.ast_1','tumor.ast_2','tumor.ast_3')
 #--------------------biomart
 Sys.setenv("http_proxy"="http://my.proxy.org:9999")
 listMarts()
@@ -76,23 +77,31 @@ head(genes)
 colnames(genes)[2]<-'Gene'
 colnames(genes)
 output <- merge(genes, matrix, by='Gene')
+colnames(output)
 #---------------------------------------------------------
 output<-data.frame(output %>%
-  group_by(external_gene_name) %>%
-  summarize(T3_1 = sum(T3_1),
-            T3_2 = sum(T3_2),
-            T3_3 = sum(T3_3),
-            PBS_1 = sum(PBS_1),
-            PBS_2 = sum(PBS_2),
-            PBS_3 = sum(PBS_3)))
+                     group_by(external_gene_name) %>%
+                     summarize(norm.ast_1 = sum(norm.ast_1),
+                               norm.ast_2 = sum(norm.ast_2),
+                               norm.ast_3 = sum(norm.ast_3),
+                               tumor.ast_1 = sum(tumor.ast_1),
+                               tumor.ast_2 = sum(tumor.ast_2),
+                               tumor.ast_3 = sum(tumor.ast_3)))
 row.names(output)<-output$external_gene_name
 output<-output[-1,-1]
-setwd('/home/em_b/work_stuff/chipseq2/rna')
-#write.csv(output,file='T3_rnaseq_df.csv')
+genes<-row.names(output)
+genes
+genes<-genes[!grepl('Gm',genes)]
+genes<-genes[!grepl('Rik',genes)]
+genes
+output<-subset(output,row.names(output)%in%genes)
+row.names(output)
+setwd('/home/em_b/Desktop/dio2_il6')
+#write.csv(output,file='tumor_norm_astrocytes.csv')
 #-------------------------------------------------------------------------------
 names<-colnames(output)
 names
-condition<-c('B','B','B','A','A','A')
+condition<-c('A','A','A','B','B','B')
 type<-c('paired','paired','paired','paired','paired','paired')
 coldata<-data.frame(cbind(names,condition,type))
 row.names(coldata)<-make.names(coldata$names,
@@ -109,6 +118,21 @@ plotDispEsts(DE)
 #-------------------------------------------------------------------------------
 results<-results(DE)
 results
+
+EnhancedVolcano(results,
+                lab = row.names(results),
+                x='log2FoldChange',
+                y='pvalue',
+                title = 'Normal astrocytes vs Tumor-associated astrocytes',
+                subtitle = 'Bulk RNAseq volcano plot. p=<0.05 log2FC=>1.5',
+                legendLabels = NULL,
+                legendIconSize = -1,
+                legendPosition = 'bottom',
+                pCutoff = 0.05,
+                FCcutoff = 1.5,
+                shape = 5)
+
+break 
 x<-data.frame(results)
 x<-x[order(x$pvalue, decreasing=FALSE),]
 x<-subset(x,padj< 0.05)
