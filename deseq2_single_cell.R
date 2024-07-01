6fddf119264d9d17176dd4e9f819cad7041c7be1
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-07-01 14:44:10 -0400

    Update deseq2_single_cell.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- deseq2_single_cell.R
+++ deseq2_single_cell.R
@@ -13,13 +13,10 @@ library(Matrix)
 library(DESeq2)
 library(EnhancedVolcano)
 setwd('/home/em_b/Desktop/scRNAseq_manuscript/gbm/R2_N')
-# Read in `matrix.mtx`
 counts <- readMM("matrix.mtx.gz")
-# Read in `genes.tsv`
 genes <- read_tsv("genes.tsv.gz", col_names = FALSE)
 gene_ids <- genes$X2
 head(gene_ids)
-# Read in `barcodes.tsv`
 cell_ids <- read_tsv("barcodes.tsv.gz", col_names = FALSE)$X1
 head(cell_ids)
 rownames(counts) <- gene_ids
@@ -29,13 +26,10 @@ names<-c(paste0("norm_", 1:11883))
 colnames(matrix)<-names
 #-------------------------------------------------------------------------------
 setwd('/home/em_b/Desktop/scRNAseq_manuscript/gbm/R2_T')
-# Read in `matrix.mtx`
 counts2 <- readMM("matrix.mtx.gz")
-# Read in `genes.tsv`
 genes2 <- read_tsv("genes.tsv.gz", col_names = FALSE)
 gene_ids2 <- genes2$X2
 head(gene_ids2)
-# Read in `barcodes.tsv`
 cell_ids2 <- read_tsv("barcodes.tsv.gz", col_names = FALSE)$X1
 head(cell_ids2)
 rownames(counts2) <- gene_ids2
@@ -44,23 +38,19 @@ matrix2<-data.frame(counts2)
 names2<-c(paste0("tumor_", 1:15094))
 colnames(matrix2)<-names2
 #-----Even out files here
-#matrix2<-matrix2[,c(1:11883)]
-matrix<-matrix[,c(1:2000)]
-matrix2<-matrix2[,c(1:2000)]
+matrix<-matrix[,c(1:1000)]
+matrix2<-matrix2[,c(1:1000)]
 #-------------------------------------------------------------------------------
 Matrix<-cbind(matrix,matrix2)
+#---must add 1 to data frame
 Matrix<-Matrix+1
-#rm(counts,counts2,genes,genes2,matrix,matrix2)
-#gc()
-break
+rm(counts,counts2,genes,genes2,matrix,matrix2)
+gc()
 names<-colnames(Matrix)
-dim(matrix)
-dim(matrix2)
-condition<-c(replicate(2000,'normal'),replicate(2000,'tumor'))
+condition<-c(replicate(1000,'normal'),replicate(1000,'tumor'))
 str(condition)
 dim(Matrix)
-type<-replicate(4000, 'paired')
-#rm(counts,counts2,genes,genes2,matrix,matrix2)
+type<-replicate(2000, 'paired')
 gc()
 coldata<-data.frame(cbind(names,condition,type))
 row.names(coldata)<-make.names(coldata$names,
@@ -76,33 +66,37 @@ deseq<-DESeqDataSetFromMatrix(countData = Matrix,
                               design = ~ condition)
 gc()
 deseq
-#rm(Matrix)
-#gc()
+deseq<-estimateSizeFactors(deseq)
 DE<-DESeq(deseq,
-          test = c('Wald'),
-          parallel = TRUE)
+          test = c('LRT'),
+          useT = TRUE,
+          fitType = 'glmGamPoi',
+          minmu = 1e-6,
+          minReplicatesForReplace = Inf,
+          reduced = ~ 1)
+resLRT <- results(DE)
+resLRT<-data.frame(resLRT)
+resLRT
 plotMA(DE,ylim=c(-5,5))
 plotDispEsts(DE)
 #-------------------------------------------------------------------------------
-results<-results(DE)
-results
-results_df<-data.frame(results)
+results_df<-data.frame(resLRT)
 results_df<-results_df[order(results_df$padj, decreasing=FALSE),]
 results_df<-subset(results_df,padj< 0.05)
 summary(results_df)
 #-------------------------------------------------------------------------------
-EnhancedVolcano(results,
-                lab = row.names(results),
+EnhancedVolcano(resLRT,
+                lab = row.names(resLRT),
                 x='log2FoldChange',
                 y='pvalue',
-                title = "",
-                subtitle = '',
+                title = 'Differential gene expressions of glioblastoma tumors compared with adjacent tissues',
+                subtitle = 'Likelihood ratio test | n=2000 cells',
                 legendLabels = NULL,
                 legendIconSize = -1,
                 legendPosition = 'bottom',
                 pCutoff = 0.001,
                 FCcutoff = 0.2,
                 shape = 1,
-                ylim = c(0,50),
-                xlim = c(-0.5,0.5))
+                ylim = c(0,80),
+                xlim = c(-1,1))
 
