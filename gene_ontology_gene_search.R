a8c97fda5b431340858ea24882876f2f2d9020d6
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-08-09 10:03:03 -0400

    Update gene_ontology_gene_search.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- gene_ontology_gene_search.R
+++ gene_ontology_gene_search.R
@@ -4,3 +4,9 @@ pathway<-select(org.Mm.eg.db,
                 columns = c('SYMBOL','GENENAME','ENTREZID'))
 list<-pathway$SYMBOL
 list<-unique(list)
+
+#----remove na's to construct seurat object 
+gnps<-gnps[c(list),]
+gnps<-rmdup(gnps)
+gnps<-na.omit(gnps)
+data <- CreateSeuratObject(counts=gnps,project = 'mb_gnp_hedgehog')
