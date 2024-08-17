af4b7259f178bd6a8372aaa843e6265679f75d87
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-08-16 23:45:05 -0400

    Update multiome_processing.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- multiome_processing.R
+++ multiome_processing.R
@@ -4,4 +4,6 @@ fastq-dump SRR26312478.sra --split-files
 #--Read 2 is the RNA insert (~91 bp)
 #--get 2 index files and 2 read files and rename and format for cellranger arc:
 #--rnaseq_S1_L001_I1_001.fastq rnaseq_S1_L001_I2_001.fastq rnaseq_S1_L001_R1_001.fastq rnaseq_S1_L001_R2_001.fastq
-pigz -p 20 <all files>
+#--place all files into folder names 'rna'
+pigz -p 20 <all rna files>
+
