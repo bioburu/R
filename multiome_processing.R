57c9c5b22282003c7220ce18b0c3b343c1d012f0
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-08-17 00:36:37 -0400

    Update multiome_processing.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- multiome_processing.R
+++ multiome_processing.R
@@ -11,3 +11,4 @@ pigz -p 20 <all rna files>
 #-- after cellranger-atac mkfastq, there four fastq.gz files will be generated. 
 #-- I1, R1, R2 and R3. I1 is the 8 bp sample barcode, R1 is the forward read. 
 #-- R2 is the 16 bp 10x feature barcode and R3 is the reverse read. 
+pigz -p 20 <all atac files>
