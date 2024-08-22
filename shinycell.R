8045d73ae419e107c0607a3eebd9d764f8192608
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-08-22 13:55:09 -0400

    Update shinycell.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- shinycell.R
+++ shinycell.R
@@ -1,16 +1,16 @@
 #----make shiny app with object of choice. 
 library(Seurat)
 library(ShinyCell)
-setwd('/home/deviancedev01/Desktop')
-data<-readRDS('/home/deviancedev01/work_stuff/multiome_MB_tumorigenesis/rna_p7.rds')
+data<-readRDS('/home/deviancedev01/work_stuff/multiome_MB_tumorigenesis/rna_adult.rds')
 scConf<-createConfig(data)
 makeShinyApp(data,
              scConf,
              gene.mapping = TRUE,
-             shiny.title = 'GSE243609:Medulloblastoma_tumorigenesis:P7:Ptch1-Atoh1EGFPmice')
+             shiny.title = 'GSE243609:Medulloblastoma_tumorigenesis:adult:Ptch1-Atoh1EGFPmice',
+             shiny.dir = '/home/deviancedev01/work_stuff/multiome_MB_tumorigenesis/adult_mb/adult_shinyapp')
 #-----deploy app on shinyapps.io
 library(rsconnect)
 rsconnect::setAccountInfo(name='9a17md-chakkapong-burudpakdee',
                           token='EB963E0629DB74B4F40C8CE2621114B3',
                           secret='deDqnsWWrRZfMUby+x+4ppKNwS2BJ0Ns+IigjjLV')
-rsconnect::deployApp('/home/deviancedev01/Desktop/shinyApp')
+rsconnect::deployApp('/home/deviancedev01/work_stuff/multiome_MB_tumorigenesis/adult_mb/adult_shinyapp')
