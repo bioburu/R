16bc8d49e95e4f58bfa2e4fe4ad9f5d8b944562c
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-08-22 12:19:34 -0400

    Update shinycell.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- shinycell.R
+++ shinycell.R
@@ -1,3 +1,4 @@
+#----make shiny app with object of choice. 
 library(Seurat)
 library(ShinyCell)
 setwd('/home/deviancedev01/Desktop')
@@ -6,4 +7,10 @@ scConf<-createConfig(data)
 makeShinyApp(data,
              scConf,
              gene.mapping = TRUE,
-             shiny.title = 'MB_tumors_ptch1.mth1_p7_npcs')
+             shiny.title = 'GSE243609:Medulloblastoma_tumorigenesis:P7:Ptch1-Atoh1EGFPmice')
+#-----deploy app on shinyapps.io
+library(rsconnect)
+rsconnect::setAccountInfo(name='9a17md-chakkapong-burudpakdee',
+                          token='EB963E0629DB74B4F40C8CE2621114B3',
+                          secret='deDqnsWWrRZfMUby+x+4ppKNwS2BJ0Ns+IigjjLV')
+rsconnect::deployApp('/home/deviancedev01/Desktop/shinyApp')
