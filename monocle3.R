75c7e7be5134830b49050b359ade35e4f63f5d0d
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-08-25 00:17:09 -0400

    Update monocle3.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- monocle3.R
+++ monocle3.R
@@ -14,82 +14,124 @@ library(org.Hs.eg.db)
 library(DESeq2)
 library(SeuratWrappers)
 library(monocle3)
-cat('GSE234832. Patient-derived brain metastasis models')
-setwd('/home/em_b/work_stuff/brain_metastasis/GSE234832_RAW')
-barcodes_path <- 'GSM7475327_LUBMET7.barcodes.tsv.gz'
-features_path <- 'GSM7475327_LUBMET7.features.tsv.gz'
-matrix_path <- 'GSM7475327_LUBMET7.matrix.mtx.gz'
+library(plotly)
+setwd('/home/deviancedev01/work_stuff/human_develop_timecourse/GSE217511_RAW')
+#-------------------------------------------------------------------------------
+barcodes_path <- 'GSM6720852_9C_barcodes.tsv.gz'
+features_path <- 'GSM6720852_9C_features.tsv.gz'
+matrix_path <- 'GSM6720852_9C_matrix.mtx.gz'
 matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
-data1 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'lung_cancer')
+data1 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = '17wks')
+data1<-subset(x = data1, downsample = 3000)
+#-------------------------------------------------------------------------------
+barcodes_path <- 'GSM6720856_26C_barcodes.tsv.gz'
+features_path <- 'GSM6720856_26C_features.tsv.gz'
+matrix_path <- 'GSM6720856_26C_matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+data2 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = '19wks')
+data2<-subset(x = data2, downsample = 3000)
+#-------------------------------------------------------------------------------
+barcodes_path <- 'GSM6720860_56C_barcodes.tsv.gz'
+features_path <- 'GSM6720860_56C_features.tsv.gz'
+matrix_path <- 'GSM6720860_56C_matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+data3 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = '20wks')
+data3<-subset(x = data3, downsample = 3000)
+#-------------------------------------------------------------------------------
+barcodes_path <- 'GSM6720864_30C_barcodes.tsv.gz'
+features_path <- 'GSM6720864_30C_features.tsv.gz'
+matrix_path <- 'GSM6720864_30C_matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+data4 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = '22wks')
+data4<-subset(x = data4, downsample = 3000)
+#-------------------------------------------------------------------------------
+barcodes_path <- 'GSM6720874_3C_barcodes.tsv.gz'
+features_path <- 'GSM6720874_3C_features.tsv.gz'
+matrix_path <- 'GSM6720874_3C_matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+data5 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = '26wks')
+data5<-subset(x = data5, downsample = 3000)
+#-------------------------------------------------------------------------------
+barcodes_path <- 'GSM6720876_60C_barcodes.tsv.gz'
+features_path <- 'GSM6720876_60C_features.tsv.gz'
+matrix_path <- 'GSM6720876_60C_matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+data6 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = '32wks')
+data6<-subset(x = data6, downsample = 3000)
+#-------------------------------------------------------------------------------
+barcodes_path <- 'GSM6720880_6C_barcodes.tsv.gz'
+features_path <- 'GSM6720880_6C_features.tsv.gz'
+matrix_path <- 'GSM6720880_6C_matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+data7 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = '38wks')
+data7<-subset(x = data7, downsample = 3000)
+#-------------------------------------------------------------------------------
+barcodes_path <- 'GSM6720882_31C_barcodes.tsv.gz'
+features_path <- 'GSM6720882_31C_features.tsv.gz'
+matrix_path <- 'GSM6720882_31C_matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+data8 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = '28yrs')
+data8<-subset(x = data8, downsample = 3000)
+#-------------------------------------------------------------------------------
+barcodes_path <- 'GSM6720884_2C_barcodes.tsv.gz'
+features_path <- 'GSM6720884_2C_features.tsv.gz'
+matrix_path <- 'GSM6720884_2C_matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+data9 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = '45yrs')
+data9<-subset(x = data9, downsample = 3000)
+#-------------------------------------------------------------------------------
+barcodes_path <- 'GSM6720886_20C_barcodes.tsv.gz'
+features_path <- 'GSM6720886_20C_features.tsv.gz'
+matrix_path <- 'GSM6720886_20C_matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+data10 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = '53yrs')
+data10<-subset(x = data10, downsample = 3000)
+data<-merge(data1,y=c(data3,data4,data5,data7,data8,data9,data10))
+table(data@meta.data$orig.ident)
 #---------------------------------------------------------------------------
-setwd('/home/em_b/work_stuff/brain_metastasis/GSE234832_RAW')
-barcodes_path <- 'GSM7475328_LUBMET1.barcodes.tsv.gz'
-features_path <- 'GSM7475328_LUBMET1.features.tsv.gz'
-matrix_path <- 'GSM7475328_LUBMET1.matrix.mtx.gz'
-matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
-data2 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'lung_cancer')
-data<-merge(data1,data2)
-table(data2@active.ident)
 data[["percent.mt"]] <- PercentageFeatureSet(data, pattern = "^MT-")
-VlnPlot(data, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol=3)
-data <- subset(data, subset = nFeature_RNA > 200 & nFeature_RNA < 7500 & percent.mt <10)
-VlnPlot(data, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol=3)
+data <- subset(data, subset = nFeature_RNA > 200 & nFeature_RNA < 9000 & percent.mt <20)
 FeatureScatter(data, feature1 = "nCount_RNA", feature2 = "nFeature_RNA") + geom_smooth()
 data <- NormalizeData(data)
 data <- FindVariableFeatures(data, selection.method = "vst", nfeatures = 2000)
-VariableFeatures(data)
-top1000 <- head(VariableFeatures(data), 1000)
-head(top1000)
-gc()
 plot1 <- VariableFeaturePlot(data)
 plot1
 all.genes <- rownames(data)
-head(all.genes)
 data <- ScaleData(data, features = all.genes)
-dim(data)
 data <- RunPCA(data, features = VariableFeatures(object = data))
 DimHeatmap(data, dims = 1:15, cells = 500, balanced = T)
 ElbowPlot(data)
+data<-JoinLayers(data)
+rm(data1,data2,data3,data4,data5,
+   data6,data7,data8,data9,data10,matrix)
+gc()
 data <- FindNeighbors(data, dims = 1:15)
-data <- FindClusters(data, resolution = 0.6)
+data <- FindClusters(data, resolution = 0.05)
 data <- RunUMAP(data, dims = 1:15)
-data <-JoinLayers(data)
+#-----For gene searches and labeling -------------------------------------------
 DimPlot(data,
-        reduction = 'umap',
-        label=TRUE,
-        label.size = 6,
-        repel = TRUE,
-        label.box = FALSE,
-        raster=FALSE,
-        pt.size = 0.5,
-        seed=1,
-        cols.highlight = c('grey'),
-        dims = c(1,2))
-#-------------------------------------------------------------------------------
-data_subset<-subset(data,ident=c(4,6,9))
-data_subset <- FindNeighbors(data_subset, dims = 1:15)
-data_subset <- FindClusters(data_subset, resolution = 0.7)
-data_subset <- RunUMAP(data_subset, dims = 1:15)
-new_idents<-c('OPC','intermediates','reactive_OPC','oligodendrocytes','astrocytes')
-names(new_idents) <- levels(data_subset)
-data_subset <- RenameIdents(data_subset, new_idents)
-levels(data_subset)
-DimPlot(data_subset,
-        reduction = 'umap',
-        label=TRUE,
-        label.size = 6,
-        repel = TRUE,
-        label.box = FALSE,
-        raster=FALSE,
-        pt.size = 1,
-        seed=1,
-        cols.highlight = c('grey'),
-        dims = c(1,2))
+         reduction = 'umap',
+         label=TRUE,
+         label.size = 3,
+         label.box = FALSE,
+         raster=FALSE,
+         pt.size = 0.1,
+         seed=1,
+         cols.highlight = c('black'),
+         dims = c(1,2))#+NoLegend()
+new_idents<-c('excitatory_neurons2','inhibitory_neurons','early_neurons','excitatory_neurons1','opc',
+              'oligodendrocytes','bipolar_neurons','mature_neurons','microglia','astrocytes')
+names(new_idents) <- levels(data)
+data <- RenameIdents(data, new_idents)
+levels(data)
 DoHeatmap(
-  data_subset,
-  features = c('GFAP','S100B','SLC1A3','IL6ST','OLIG2','ZNF488','MOG','VIM','CD44','LAMP2'),
+  data,
+  features = c('OLIG1','OLIG2','SOX2','SOX9','MOG',
+               'GFAP','S100B','SLC1A2',
+               'RBFOX3','NEUROD2','CALM1',
+               'AGBL4','PTPRD','MSR1','TCF4'),
   cells = NULL,
-  group.by = "ident",
+  #group.by = "cell_types",
   group.bar = TRUE,
   group.colors = NULL,
   disp.min = -2.5,
@@ -97,141 +139,75 @@ DoHeatmap(
   slot = "scale.data",
   assay = NULL,
   label = TRUE,
-  size = 5.5,
+  size = 3.5,
   hjust = 0,
   vjust = 0,
-  angle = 45,
+  angle = 50,
   raster = TRUE,
   draw.lines = TRUE,
   lines.width = NULL,
   group.bar.height = 0.02,
   combine = TRUE
 )
-VlnPlot(data_subset, features = c('OLIG2','SOX2','SOX9','SOX10','MOG','VIM',
-                                  'GFAP','S100B','SLC1A2','SLC1A3','S100A10',
-                                  'MAP2','NOTCH1','TUBB2B','GAS1','PDGFA'),
-        cols = c())
-VlnPlot(data_subset, features = c('GFAP','EZH2','SUZ12','THRA','S100B','IL6ST'))#,
-        #cols = c('red'))
-#-------------------------------------------------------------------------------
-cds <- as.cell_data_set(data_subset)
-cds
+
+
+table(Idents(data))
+data@meta.data$cell_types<-Idents(data)
+head(data@meta.data)
+#----switch active ident to time course
+Idents(data)<-data@meta.data$orig.ident
+table(data@active.ident)
+cds <- as.cell_data_set(data)
 head(colData(cds))
-#-------------------------------------------------------------------------------
 #----Edit partitions layer
-cds@clusters@listData$UMAP$partitions
 names(cds@clusters@listData$UMAP$partitions) <- cds@colData@rownames
-cds
-cds@clusters@listData
-cds@clusters@listData$UMAP$clusters<-data_subset@active.ident
+#cds
+#cds@clusters@listData
+#data@active.ident<-data@active.ident
+cds@clusters@listData$UMAP$clusters<-data@active.ident
+#----learn trajectory
 cds<-learn_graph(cds,
-                 use_partition = FALSE)
-head(clusters(cds))
+                 use_partition = TRUE)
+#head(clusters(cds))
+#table(clusters(cds))
+#----order cells from initial starting point
 cds <- order_cells(cds, reduction_method = "UMAP",
-                   root_cells = colnames(cds[,clusters(cds)=='reactive_OPC']))
-p1<-plot_cells(cds,
-           reduction_method = c('UMAP'),
+                   root_cells = colnames(cds[,clusters(cds)=='17wks']))
+#-----plots
+plot_cells(cds,reduction_method = c('UMAP'),
            show_trajectory_graph = TRUE,
            trajectory_graph_color = 'red',
-           group_label_size = 10,
-           cell_size = 1,
+           group_label_size = 10,cell_size = 1,
            color_cells_by = c('pseudotime'))
-p2<-DimPlot(data_subset,
-        reduction = 'umap',
-        label=TRUE,
-        label.size = 6,
-        repel = TRUE,
-        label.box = FALSE,
-        raster=FALSE,
-        pt.size = 1,
-        seed=1,
-        cols.highlight = c('grey'),
-        dims = c(1,2))
-p1+p2
-VlnPlot(data_subset, features = c('SOX2','MYC','CD44'))
-gene_fits<-fit_models(cds,model_formula_str = '~pseudotime')
-head(gene_fits)
-#----fit_coefs show genes which vary as a function of time
-fit_coefs<-coefficient_table(gene_fits)
-fit_coefs <- fit_coefs %>% filter(term == 'pseudotime')
-fit_coefs<-subset(fit_coefs,q_value< 0.05)
-fit_coefs<-fit_coefs[,c(2,6,7,13)]
-#------pseudotime vs gene expression levels
-cds_subset<-cds[row.names(subset(rowData(cds),
-                                 row.names(rowData(cds))%in%c('OLIG2','SOX9','GFAP','VIM','S100B','MOG','SPP1','TF',
-                                                              'NGFR','DPYSL5','TBC1D12','GSN','CAVIN1','ELOVL1','MARCKS'))),]
-cds_subset
-colData(cds_subset)$pseudotime<-pseudotime(cds_subset,reduction_method = 'UMAP')
-colData(cds_subset)
-plot_genes_violin(cds_subset,
-                  group_cells_by="pseudotime",
-                  ncol=3,
-                  nrow = 5,
-                  label_by_short_name = FALSE,
-                  normalize = FALSE,
-                  log_scale = FALSE)
-#-------------------------------------------------------------------------------
-cds_subset<-cds[row.names(subset(rowData(cds),
-                                 row.names(rowData(cds))%in%c('HAPLN2','IER2','DNAJB1','BACE1','NRBP2',
-                                                              'RAPGEF5','PMP2','MT3','CLU','GPR37L1'))),]
-cds_subset
-colData(cds_subset)$pseudotime<-pseudotime(cds_subset,reduction_method = 'UMAP')
-colData(cds_subset)
-plot_genes_violin(cds_subset,
-                  group_cells_by="pseudotime",
-                  ncol=2,
-                  label_by_short_name = FALSE,
-                  normalize = FALSE,
-                  log_scale = FALSE)
-cds_pr_test<-graph_test(cds,neighbor_graph = 'principal_graph',cores = 4)
-cds_pr_test<-subset(cds_pr_test,q_value<0.05)
-cds_pr_test<-cds_pr_test[order(cds_pr_test$morans_I, decreasing=TRUE),]
-rowData(cds)$gene_short_name<-row.names(rowData(cds))
-rowData(cds)
-head(pseudotime(cds), 10)
 cds$monocle3_pseudotime <- pseudotime(cds)
-data.pseudo <- as.data.frame(colData(cds))
-ggplot(data.pseudo, aes(monocle3_pseudotime, reorder(Idents(data_subset), monocle3_pseudotime), fill = seurat_clusters)) + geom_boxplot()
-#----------------
-data_subset <- AddMetaData(
-  object = data_subset,
-  metadata = cds@principal_graph_aux@listData$UMAP$pseudotime,
-  col.name = "astrocyte_trajectory"
-)
-FeaturePlot(data_subset, c("astrocyte_trajectory"), pt.size = 1) & scale_color_viridis_c()
-FeaturePlot(data_subset,
-            features = c(row.names(cds_pr_test)[1:20]))
-VlnPlot(data_subset, features = c('MARCKS','CLU','S100B','MT3','TUBB2B','PLP1',
-                                  'FOSB','GPR37L1','SLC1A3','HSPA1B',
-                                  'EGR1','DNAJB1','SPP1','HAPLN2','TF',
-                                  'IER2'))
-# a helper function to identify the root principal points:
-summary(cds@colData$seurat_clusters)
-#---time bin is the earliest group assigned by user from colData metadata
-get_earliest_principal_node <- function(cds, time_bin="0"){
-  cell_ids <- which(colData(cds)[, "seurat_cluster"] == time_bin)
-  closest_vertex <-cds@principal_graph_aux[["UMAP"]]$pr_graph_cell_proj_closest_vertex
-  closest_vertex <- as.matrix(closest_vertex[colnames(cds), ])
-  root_pr_nodes <-igraph::V(principal_graph(cds)[["UMAP"]])$name[as.numeric(names(which.max(table(closest_vertex[cell_ids,]))))]
-  root_pr_nodes
-}
 cds_3d <- reduce_dimension(cds, max_components = 3)
 cds_3d <- cluster_cells(cds_3d)
 cds_3d <- learn_graph(cds_3d)
-#cds_3d <- order_cells(cds_3d, root_pr_nodes=get_earliest_principal_node(cds))
-plot_cells(cds,
-           color_cells_by = "seurat_clusters",
-           label_cell_groups=TRUE,
-           group_label_size = 10,
-           label_leaves=FALSE,
-           label_branch_points=FALSE,
-           graph_label_size=2,
-           cell_size = 1)
+cds_3d@colData$monocle3_pseudotime
+data@meta.data$monocle_pseudotime<-cds_3d@colData$monocle3_pseudotime
+head(data@meta.data)
+#---- 3d plot rna 
+data3d <- data
+data3d <- RunUMAP(data3d,dims = 1:15,n.components = 3L)
+#head(Embeddings(data3d,reduction = "umap"))
+head(data3d@meta.data)
+plot3d1 <- FetchData(data3d, vars = c("umap_1", "umap_2", "umap_3", "cell_types","monocle_pseudotime"))
+plot3d1$label <- paste(plot3d1$cell_types)
+fig <- plot_ly(data = plot3d1, 
+               x = ~umap_1, y = ~umap_2, z = ~umap_3, 
+               color = ~monocle_pseudotime, 
+               colors = c('viridis'),
+               type = "scatter3d", 
+               mode = "markers", 
+               marker = list(size = 2, width=2),
+               text=~label,
+               hoverinfo="text")%>%layout(title='Monocle3')
+fig
 plot_cells_3d(
   cds_3d,
   dims = c(1, 2, 3),
-  reduction_method = c("UMAP", "tSNE", "PCA", "LSI", "Aligned"),
-  color_cells_by = "monocle3_pseudotime",
+  reduction_method = c('PCA'),
+  color_cells_by = "orig.ident",
   genes = NULL,
   show_trajectory_graph = FALSE,
   trajectory_graph_color = "red",
@@ -239,25 +215,6 @@ plot_cells_3d(
   norm_method = c("log"),
   color_palette = NULL,
   color_scale = "Viridis",
-  cell_size =200 ,
+  cell_size =50 ,
   alpha = 1,
-  min_expr = 0.1
-)
-cds_3d@colData
-plot_cells_3d(
-  cds_3d,
-  dims = c(1, 2, 3),
-  reduction_method = c("UMAP", "tSNE", "PCA", "LSI", "Aligned"),
-  color_cells_by = "ident",
-  genes = NULL,
-  show_trajectory_graph = FALSE,
-  trajectory_graph_color = "red",
-  trajectory_graph_segment_size = 10,
-  norm_method = c("log"),
-  color_palette = NULL,
-  color_scale = "Viridis",
-  cell_size =200 ,
-  alpha = 1,
-  min_expr = 0.1
-)
-break 
+  min_expr = 0.1)%>%layout(title='PCA')
