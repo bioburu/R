6999da95caf84cccac55134164be8a2d51dbb994
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-09-05 21:55:37 -0400

    Update remove_pseudo_ genes.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- remove_pseudo_ genes.R
+++ remove_pseudo_ genes.R
@@ -2,3 +2,7 @@ both_common
 both_common<-both_common[!grepl('Gm',both_common)]
 both_common
 both_common<-both_common[!grepl('Rik',both_common)]
+
+#---for row.names
+markers<-markers[-grep("Rik", row.names(markers)),]
+markers<-markers[-grep("mt-", row.names(markers)),]
