8e5bb5dd1fe5d35f1c3a8276ea87c669cffb92a9
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-09-06 16:36:38 -0400

    Update h2o.glm.rf.gbm.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- h2o.glm.rf.gbm.R
+++ h2o.glm.rf.gbm.R
@@ -1,5 +1,3 @@
-# required packages
-# install vip from github repo: devtools::install_github("koalaverse/vip")
 library(lime)       # ML local interpretation
 library(vip)        # ML global interpretation
 library(pdp)        # ML global interpretation
@@ -15,6 +13,7 @@ h2o.init()
 df<-read.csv('/home/deviancedev01/Desktop/interesting_exps/model_explaining/WA_Fn-UseC_-Telco-Customer-Churn.csv',
              stringsAsFactors = TRUE)%>%mutate_if(is.numeric,round)%>%mutate_if(is.numeric, as.integer)
 df$customerID<-NULL
+df$SeniorCitizen<-NULL
 str(df)
 #---unorder ordered factors and change attrition to factor class w levels
 #df <- df %>% 
@@ -39,31 +38,66 @@ train_obs.h2o <- as.h2o(train_obs)
 train_obs.h2o
 local_obs.h2o <- as.h2o(local_obs)
 local_obs.h2o
-#----randomforest
-h2o_rf <- h2o.randomForest(x, y, training_frame = train_obs.h2o)
-h2o_rf
 #----logistic regression
 h2o_glm <- h2o.glm(x, y, training_frame = train_obs.h2o, family = "binomial")
 h2o_glm
-#-----gradient boosting tree model
-h2o_gbm <- h2o.gbm(x, y, training_frame = train_obs.h2o)
-h2o.gbm
-#---partial dependency plot for random forest
-h2o.partialPlot(h2o_rf, data = train_obs.h2o, cols = "MonthlyCharges")
 #---partial dependency plot for regression
-h2o.partialPlot(h2o_glm, data = train_obs.h2o, cols = "MonthlyCharges")
-#---partial dependency plot for gradient boosting tree model
-h2o.partialPlot(h2o_gbm, data = train_obs.h2o, cols = "MonthlyCharges")
+h2o.partialPlot(h2o_glm,
+                newdata = train_obs.h2o,
+                cols = "MonthlyCharges",
+                plot=TRUE,
+                plot_stddev=TRUE)
+explainer_h2o_glm <- lime(train_obs,
+                  h2o_glm,
+                  bin_continuous = TRUE,
+                  quantile_bins = FALSE)
+explanation_glm <- lime::explain(local_obs[1:6,],
+                                 explainer_h2o_glm,
+                             n_labels = 1,
+                             n_features = 3)
+h2o.confusionMatrix(h2o_glm, local_obs.h2o, valid = FALSE, xval = FALSE)
+plot_features(explanation_glm)
+plot_explanations(explanation_glm)
+#----randomforest
+h2o_rf <- h2o.randomForest(x, y,
+                           nfolds = 5,
+                           training_frame = train_obs.h2o)
+h2o_rf
+h2o.partialPlot(h2o_rf,
+                newdata = train_obs.h2o,
+                cols = "MonthlyCharges")
+explainer_h2o_rf  <- lime(train_obs,
+                          h2o_rf,
+                          bin_continuous = TRUE,
+                          quantile_bins = FALSE)
+explanation_rf <- lime::explain(local_obs[1:6,],
+                                explainer_h2o_rf,
+                                n_labels = 1,
+                                n_features = 3)
+h2o.confusionMatrix(h2o_rf,local_obs.h2o, valid = FALSE, xval = FALSE)
+plot_features(explanation_rf)
+plot_explanations(explanation_rf)
 
-explainer_h2o_rf  <- lime::lime(train_obs, h2o_rf, n_bins = 5)
-explainer_h2o_glm <- lime::lime(train_obs, h2o_glm, n_bins = 5)
-explainer_h2o_gbm <- lime::lime(train_obs, h2o_gbm, n_bins = 5)
+#-----gradient boosting tree model
+h2o_gbm <- h2o.gbm(x, y,
+                   training_frame = train_obs.h2o,
+                   nfolds = 5)
+head(h2o.gbm)
+h2o.partialPlot(h2o_gbm,
+                data = train_obs.h2o,
+                cols = "MonthlyCharges")
+explainer_h2o_gbm  <- lime(train_obs,
+                          h2o_gbm,
+                          bin_continuous = TRUE,
+                          quantile_bins = FALSE)
+explanation_gbm <- lime::explain(local_obs[1:6,],
+                                 explainer_h2o_gbm,
+                                 n_labels = 1,
+                                 n_features = 3)
+plot_features(explanation_gbm)
+plot_explanations(explanation_gbm)
 
-explanation_rf <- lime::explain(local_obs[1:2,], explainer_h2o_rf, n_features = 5, labels = "Yes", kernel_width = .1, feature_select = "highest_weights")
-explanation_glm <- lime::explain(local_obs[1:2,], explainer_h2o_glm, n_features = 5, labels = "Yes", kernel_width = .1, feature_select = "highest_weights")
-explanation_gbm <- lime::explain(local_obs[1:2,], explainer_h2o_gbm, n_features = 5, labels = "Yes", kernel_width = .1, feature_select = "highest_weights")
-
-p1 <- plot_features(explanation_rf, ncol = 1) + ggtitle("rf")
-p2 <- plot_features(explanation_glm, ncol = 1) + ggtitle("glm")
-p3 <- plot_features(explanation_gbm, ncol = 1) + ggtitle("gbm")
+p1 <- plot_features(explanation_rf[4:6,], ncol = 1) + ggtitle("rf")
+p2 <- plot_features(explanation_glm[4:6,], ncol = 1) + ggtitle("glm")
+p3 <- plot_features(explanation_gbm[4:6,], ncol = 1) + ggtitle("gbm")
 gridExtra::grid.arrange(p1, p2, p3, nrow = 1)
