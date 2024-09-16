11b2f8a1ad17caf39afa4a860c53da5e2cc49e39
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-09-16 14:12:43 -0400

    Update and rename Modeler.R to quotes4strings.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- Modeler.R
+++
@@ -1,4 +0,0 @@
-x<-FindMarkers(data, ident.1 = '', ident.2 = '', 
-               features = c(),logfc.threshold=1.5,only.pos = TRUE,test.use='DESeq2')
-VlnPlot(data, features = c(row.names(x)[1:12]),cols = c('grey','red'))
-write.csv(x,file = '')
---
+++ quotes4strings.R
@@ -0,0 +1,2 @@
+TEMP <- scan(text=virusNtest, what="")
+
