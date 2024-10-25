5ee8baa348a8f068da1f4c7e8211d06329c52a2e
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-10-24 22:21:02 -0400

    Create get_kegg_genes.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

---
+++ get_kegg_genes.R
@@ -0,0 +1,11 @@
+matrix<-read.csv('/home/deviancedev01/Desktop/prion_disease_kegg.csv',
+                 header = FALSE)
+v<-matrix$V1
+library(stringr)
+v
+v2<-str_extract(v, "[^;]+")
+v2<-na.omit(v2)
+v2<-v2[-c(70,275:277)]
+v2
+cat(v2)
+write.csv(v2,file = '/home/deviancedev01/Desktop/prion_disease_kegg.csv')
