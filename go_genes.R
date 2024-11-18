dcb49cc3f53fbdc03abf14699079e679bb7ac248
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-11-18 17:06:00 -0500

    Update go_genes.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- go_genes.R
+++ go_genes.R
@@ -4,4 +4,6 @@ retrieved_genes <- AnnotationDbi::select(org.Mm.eg.db,
                                    keytype="GOALL",
                                    keys="GO:0030054",
                                    columns=c('SYMBOL','GENENAME','ENTREZID'))
-retrieved+genes
+list<-retrieved_genes$SYMBOL
+list<-unique(list)
+list
