dac364c490cd0d264763824d6c6a1118b7d119f0
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-11-18 17:47:06 -0500

    Delete csv_import_options.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- csv_import_options.R
+++
@@ -1,8 +0,0 @@
-#--example 1
-biopsy<-read.csv('/home/deviancedev01/Downloads/WA_Fn-UseC_-Telco-Customer-Churn.csv',
-                 stringsAsFactors = TRUE)%>%mutate_if(is.numeric, round)%>%mutate_if(is.numeric,as.integer)
-biopsy$customerID <- NULL
-biopsy <- na.omit(biopsy)
-#--to remove elements from column
-df$year<-sub(":.*", "", df$year)
-
