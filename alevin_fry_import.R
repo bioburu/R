dd6ba6adf923cf914021b83f77bb89690509c6aa
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-11-18 17:00:16 -0500

    Update and rename Alevin_fry_import.R to alevin_fry_import.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- alevin_fry_import.R
+++ alevin_fry_import.R
@@ -6,21 +6,18 @@ library(dplyr)
 library(Seurat)
 library(Matrix)
 library(readr)
-files<-'/home/deviancedev/Desktop/drive_jan2024/FCCC/scRNAseq_test/SRR27442073/mm10_quant'
+files<-'/home/mm10_quant'
 x<-loadFry(files,outputFormat = 'scRNA',nonzero = FALSE,quiet = FALSE)
 x
-#----
 gc()
 counts <- assay(x, "counts")
 counts
 dim(counts)
 df<-data.frame(counts)
 gc()
-#------
 matrix<-df
 matrix<-cbind(row.names(matrix),matrix)
 colnames(matrix)[1]<-'Gene'
-#-----
 Sys.setenv("http_proxy"="http://my.proxy.org:9999")
 listMarts()
 ensembl=useMart("ensembl")
@@ -53,13 +50,12 @@ colnames(Ftable)[1] <- c('ensembl_id')
 rm(attributes,counts,df,ensembl,filters,genes,matrix,x)
 gc()
 Ftable<-Ftable[!duplicated(Ftable$external_gene_name), ]
-setwd('/home/deviancedev/Desktop/drive_jan2024/FCCC/scRNAseq_test/SRR27442073')
 #write.csv(Ftable,file = 'GSM8004749.Sciatic.nerve.CD45p.spon.autoimmune.neuropathy.csv')
 features<-Ftable[,c(1:2)]
 matrix.mtx<-Ftable[,-c(1:7)]
 matrix.mtx<-as(matrix.mtx,'sparseMatrix')
 barcodes<-data.frame(colnames(Ftable)[8:8800])
-setwd('/home/deviancedev/Desktop/drive_jan2024')
+setwd('/home/drive')
 write_tsv(features,file='features.tsv',col_names = NA)
 write_tsv(barcodes,file='barcodes.tsv',col_names = NA)
 writeMM(matrix.mtx,file='matrix.mtx')
