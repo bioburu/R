e7c5d754db49d2473deb8788ac0029dff277a791
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-11-18 17:24:43 -0500

    Update and rename RNAseq_ChIPseq_igv_template.R to rnaseq_chipseq_binning.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- rnaseq_chipseq_binning.R
+++ rnaseq_chipseq_binning.R
@@ -20,7 +20,7 @@ library(BRGenomics)
 library(ggplot2)
 library(UpSetR)
 library(ChIPpeakAnno)
-setwd('/home/em_b/work_stuff/FCCC/T3_RNAseq')
+setwd('/home/T3_RNAseq')
 matrix<-read.csv('dataframe2.csv')
 colnames(matrix)
 row.names(matrix)<-make.names(matrix$Gene,unique = TRUE)
@@ -36,7 +36,6 @@ row.names(coldata)<-make.names(coldata$names,
                                unique=TRUE)
 coldata<-coldata[,-1]
 coldata
-#-----------------------------------------------------------------------------
 tester<-round(tester)
 colnames(tester)
 row.names(coldata)
@@ -45,7 +44,6 @@ deseq
 DE<-DESeq(deseq)
 plotMA(DE,ylim=c(-5,5))
 plotDispEsts(DE)
-#-------------------------------------------------------------------------------
 results<-results(DE)
 results
 x<-data.frame(results)
@@ -55,34 +53,28 @@ downreg<-subset(x,log2FoldChange< 0)
 downregulated_rna<-cbind(row.names(downreg),downreg)
 colnames(downregulated_rna)[1]<-'Gene'
 txdb<-TxDb.Mmusculus.UCSC.mm39.refGene
-bam.files<-c(H3K27me3_0hr.E1='/home/em_b/work_stuff/FCCC/chipseq/bam/E1.0h.ME3.rmdup.sort.bam',
-             #H3K27me3_0hr.G1='/home/em_b/work_stuff/FCCC/chipseq/bam/F1.0h.Tra.rmdup.sort.bam',
-             H3K27me3_0hr.I1='/home/em_b/work_stuff/FCCC/chipseq/bam/I1.0h.ME3.rmdup.sort.bam',
-             H3K27me3_t3.E3='/home/em_b/work_stuff/FCCC/chipseq/bam/E3_t3_me3.rmdup.sort.bam',
-             H3K27me3_t3.G3='/home/em_b/work_stuff/FCCC/chipseq/bam/G3.T3.Me3.rmdup.sort.bam',
-             H3K27me3_t3.I3='/home/em_b/work_stuff/FCCC/chipseq/bam/I3.T3.Me3.rmdup.sort.bam')
+bam.files<-c(H3K27me3_0hr.E1='/home/E1.0h.ME3.rmdup.sort.bam',
+             H3K27me3_0hr.G1='/home/F1.0h.Tra.rmdup.sort.bam',
+             H3K27me3_0hr.I1='/home/I1.0h.ME3.rmdup.sort.bam',
+             H3K27me3_t3.E3='/home/E3_t3_me3.rmdup.sort.bam',
+             H3K27me3_t3.G3='/home/G3.T3.Me3.rmdup.sort.bam',
+             H3K27me3_t3.I3='/home/I3.T3.Me3.rmdup.sort.bam')
 #---get paired end sizes in bam files ONLY IF NEEDED
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-bam1<-getPESizes('/home/em_b/work_stuff/FCCC/chipseq/bam/E1.0h.ME3.rmdup.sort.bam',
+bam1<-getPESizes('E1.0h.ME3.rmdup.sort.bam',
                  param=readParam(pe='both'))
-bam2<-getPESizes('/home/em_b/work_stuff/FCCC/chipseq/bam/I1.0h.ME3.rmdup.sort.bam',
+bam2<-getPESizes('I1.0h.ME3.rmdup.sort.bam',
                  param=readParam(pe='both'))
-bam3<-getPESizes('/home/em_b/work_stuff/FCCC/chipseq/bam/E3_t3_me3.rmdup.sort.bam',
+bam3<-getPESizes('E3_t3_me3.rmdup.sort.bam',
                  param=readParam(pe='both'))
-bam4<-getPESizes('/home/em_b/work_stuff/FCCC/chipseq/bam/G3.T3.Me3.rmdup.sort.bam',
+bam4<-getPESizes('G3.T3.Me3.rmdup.sort.bam',
                  param=readParam(pe='both'))
-bam5<-getPESizes('/home/em_b/work_stuff/FCCC/chipseq/bam/I3.T3.Me3.rmdup.sort.bam',
+bam5<-getPESizes('I3.T3.Me3.rmdup.sort.bam',
                  param=readParam(pe='both'))
 summary(bam1$sizes)
 summary(bam2$sizes)
 summary(bam3$sizes)
 summary(bam4$sizes)
 summary(bam5$sizes)
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
 param <- readParam(minq=30,
                    max.frag = 300,
                    pe='both')
@@ -97,14 +89,8 @@ binned <- windowCounts(bam.files,
                      width=10000,
                      param=param,
                      bin = TRUE)
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-break 
-cat('keep data and binned if starting over')
 gc()
 cat('find all upregulated h3k27me3 peaks')
-#-- the higher the filter value, the more restrictive 
 log2(5)
 keep <- filterWindowsGlobal(data=data,
                             background=binned)$filter > 0.99
@@ -156,7 +142,7 @@ length(upregulated_chip$SYMBOL)
 list<-downregulated_rna$Gene
 subset_list<-subset(upregulated_chip, SYMBOL %in% list)
 length(subset_list$SYMBOL)
-setwd('/home/em_b/work_stuff/FCCC/T3_RNAseq/')
+setwd('/home/T3_RNAseq/')
 #----------------ChIPseq heatmap  ------------------------------------------------
 chip_htmp<-subset_list
 row.names(chip_htmp)<-make.names(chip_htmp$SYMBOL,
@@ -177,7 +163,7 @@ pheatmap(chip_htmp,
          angle_col = 315,
          main = 'H3K27me3_0hr_vs_T3_ChIP_peaks')
 #---------------- RNAseq heatmap  ------------------------------------
-matrix<-read.csv('/home/em_b/work_stuff/FCCC/T3_RNAseq/dataframe2.csv')
+matrix<-read.csv('/home/T3_RNAseq/dataframe2.csv')
 list<-subset_list$SYMBOL
 list
 colnames(matrix)
@@ -200,12 +186,10 @@ pheatmap(subset,
          color = colorRampPalette(rev(brewer.pal(n=7,name='RdYlBu')))(100),
          angle_col = 315,
          main = 'MB_tumors_0hr_vs_T3_RNAseq_transcripts')
-setwd('/home/em_b/work_stuff/ChIP_RNA_comparison_templates')
+setwd('/home/ChIP_RNA_comparison_templates')
 #write.csv(subset_list,file='H3K27me3_T3_0hr_RNAChIP.csv')
 rm(coldata,counts,counts_in,Data,deseq,df,DF,matrix,peakAnno,ranges,resLRT,subset,test)
 gc()
-cat('gotcha')
-#-------------------------------------------------------------------------------
 #------DAVID pathway heatmaps RNAseq
 negative_regulation_of_neuron_differentiation<-read.delim('/home/em_b/work_stuff/ChIP_RNA_comparison_templates/davidBI_GOTerm_negative regulation of neuron differentiation.txt')
 list<-row.names(negative_regulation_of_neuron_differentiation)
@@ -253,11 +237,11 @@ pheatmap(subset[list,],
          angle_col = 315,
          main = 'GO Term: Chromatin_binding')
 #------DAVID pathway heatmaps RNAseq
-chip<-read.csv('/home/em_b/work_stuff/ChIP_RNA_comparison_templates/H3K27me3_T3_0hr_RNAChIP.csv',
+chip<-read.csv('/home/H3K27me3_T3_0hr_RNAChIP.csv',
                row.names = 1)
 row.names(chip)<-make.names(chip$SYMBOL,unique = TRUE)
 chip<-chip[,-c(1:5,11:28)]
-negative_regulation_of_neuron_differentiation<-read.delim('/home/em_b/work_stuff/ChIP_RNA_comparison_templates/davidBI_GOTerm_negative regulation of neuron differentiation.txt')
+negative_regulation_of_neuron_differentiation<-read.delim('/home/davidBI_GOTerm_negative regulation of neuron differentiation.txt')
 list<-row.names(negative_regulation_of_neuron_differentiation)
 list
 pheatmap(chip[list,],
@@ -273,7 +257,7 @@ pheatmap(chip[list,],
          color = colorRampPalette(rev(brewer.pal(n=7,name='RdYlBu')))(100),
          angle_col = 315,
          main = 'GO Term: Negative_regulation_of_neuron_differentiation')
-negative_regulation_of_transcription_from_RNA_polymerase_II_promoter<-read.delim('/home/em_b/work_stuff/ChIP_RNA_comparison_templates/davidBI_GOTerm_negative_regulation_of_transcription_from_RNA_polymerase_II_promoter.txt')
+negative_regulation_of_transcription_from_RNA_polymerase_II_promoter<-read.delim('/home/davidBI_GOTerm_negative_regulation_of_transcription_from_RNA_polymerase_II_promoter.txt')
 list<-row.names(negative_regulation_of_transcription_from_RNA_polymerase_II_promoter)
 pheatmap(chip[list,],
          scale = 'row',
@@ -288,7 +272,7 @@ pheatmap(chip[list,],
          color = colorRampPalette(rev(brewer.pal(n=7,name='RdYlBu')))(100),
          angle_col = 315,
          main = 'GO Term:Negative_regulation_of_transcription_from_RNA_polyII_promoter')
-chromatin_binding<-read.delim('/home/em_b/work_stuff/ChIP_RNA_comparison_templates/davidBI_GOTerm_chromatin_binding.txt')
+chromatin_binding<-read.delim('/home/davidBI_GOTerm_chromatin_binding.txt')
 list<-row.names(chromatin_binding)
 pheatmap(chip[list,],
          scale = 'row',
@@ -314,17 +298,17 @@ roi <- getGenomicRegion(igv)
 gr.roi <- with(roi, GRanges(seqnames=chrom, ranges = IRanges(start, end)))
 param <- ScanBamParam(which=gr.roi, what = scanBamWhat())
 #---enter in bam file
-bamFile <- '/home/em_b/work_stuff/FCCC/chipseq/bam/I1.0h.ME3.rmdup.sort.bam'
+bamFile <- '/home/I1.0h.ME3.rmdup.sort.bam'
 alignments <- readGAlignments(bamFile, use.names=TRUE, param=param)
 track <- GenomicAlignmentTrack(trackName='h3k27me3_0hr', alignments, visibilityWindow=10000000, trackHeight=200) 
 track
 displayTrack(igv, track)
-bamFile <- '/home/em_b/work_stuff/FCCC/chipseq/bam/I3.T3.Me3.rmdup.sort.bam'
+bamFile <- '/home/I3.T3.Me3.rmdup.sort.bam'
 alignments <- readGAlignments(bamFile, use.names=TRUE, param=param)
 track <- GenomicAlignmentTrack(trackName='h3k27me3_T3', alignments, visibilityWindow=10000000, trackHeight=200) 
 track
 displayTrack(igv, track)
-tbl.pk <- read.delim('/home/em_b/work_stuff/FCCC/chipseq/macs/I3/I3_C3_peaks.narrowPeak',
+tbl.pk <- read.delim('/home/I3_C3_peaks.narrowPeak',
                      header=FALSE)
 colnames(tbl.pk)<-c('chrom','start','end','name','score')
 head(tbl.pk)
@@ -332,4 +316,3 @@ tbl.pk<-tbl.pk[,-c(4,6:10)]
 unlist(lapply(tbl.pk, class))
 track <- DataFrameQuantitativeTrack("h3k27me3_T3_peaks", tbl.pk, color="red", autoscale=TRUE)
 displayTrack(igv, track)
-break 
