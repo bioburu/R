6d513b23dc85638b67a05b139809cadec0ba33fc
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-11-18 17:04:06 -0500

    Update and rename GO_pathway_parser.R to go_genes.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- go_genes.R
+++ go_genes.R
@@ -1,6 +1,7 @@
 library(org.Mm.eg.db)
-li
-retrieved <- AnnotationDbi::select(org.Mm.eg.db,
+
+retrieved_genes <- AnnotationDbi::select(org.Mm.eg.db,
                                    keytype="GOALL",
                                    keys="GO:0030054",
                                    columns=c('SYMBOL','GENENAME','ENTREZID'))
+retrieved+genes
