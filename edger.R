9ec8b67bc2af0c32fa28032c9d314061925309ca
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-11-18 17:01:38 -0500

    Update and rename EdgeR.R to edger.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- edger.R
+++ edger.R
@@ -1,5 +1,5 @@
 library(edgeR)
-matrix<-read.csv('/home/deviancedev/Desktop/drive_jan2024/FCCC/exosome_exp/bulk_tissue/csv_results/bulk_tissues_biomart.csv',row.names = 1)
+matrix<-read.csv('/home/bulk_tissues_biomart.csv',row.names = 1)
 matrix<-matrix[,-c(1:8)]
 colnames(matrix)<-c('Norm_1','Norm_2','Norm_3','MB_1','MB_2','MB_3')
 condition <- factor(c(1,1,1,2,2,2))
@@ -15,5 +15,5 @@ topTags(qlf)
 df<-data.frame(qlf)
 df<-df[order(df$PValue, decreasing=FALSE),]
 df<-subset(df,PValue< 0.05)
-#setwd('/home/deviancedev/Desktop/drive_jan2024/FCCC/exosome_exp/bulk_tissue/csv_results')
+#setwd('/home/csv_results')
 #write.csv(df,file = 'bulk_tissues_edgeR.csv')
