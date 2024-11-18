ad9c844c42926861d449b592f27399e7558eeb02
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-11-18 17:34:08 -0500

    Update and rename chip_replicate_pathway_comparisons.R to chipseq_rep_comparisons.R
    Signed-off-by: devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>

--- chipseq_rep_comparisons.R
+++ chipseq_rep_comparisons.R
@@ -3,20 +3,19 @@ library(clusterProfiler)
 library(TxDb.Mmusculus.UCSC.mm39.knownGene)
 library(org.Mm.eg.db)
 #----------single files
-files<-list(Thra_0hr_1='/home/em_b/work_stuff/chipseq/final/peaks_q_0.05/D1_J1_peaks.narrowPeak',
-            Thra_0hr_2='/home/em_b/work_stuff/chipseq/final/peaks_q_0.05/F1_K1_peaks.narrowPeak',
-            #Thra_0hr_3='/home/em_b/work_stuff/chipseq/final/peaks_q_0.05/H1_L1_peaks.narrowPeak',
-            Thra_B22_1='/home/em_b/work_stuff/chipseq/final/peaks_q_0.05/D2_J2_peaks.narrowPeak',
-            #Thra_B22_2='/home/em_b/work_stuff/chipseq/final/peaks_q_0.05/F2_K2_peaks.narrowPeak',
-            Thra_B22_3='/home/em_b/work_stuff/chipseq/final/peaks_q_0.05/H2_L2_peaks.narrowPeak',
-            Thra_T3_1='/home/em_b/work_stuff/chipseq/final/peaks_q_0.05/D3_J3_peaks.narrowPeak')#,
-            #Thra_T3_2='/home/em_b/work_stuff/chipseq/final/peaks_q_0.05/F3_K3_peaks.narrowPeak',
-            #Thra_T3_3='/home/em_b/work_stuff/chipseq/final/peaks_q_0.05/H3_L3_peaks.narrowPeak')
+files<-list(Thra_0hr_1='/home/peaks_q_0.05/D1_J1_peaks.narrowPeak',
+            Thra_0hr_2='/home/peaks_q_0.05/F1_K1_peaks.narrowPeak',
+            Thra_0hr_3='/home/peaks_q_0.05/H1_L1_peaks.narrowPeak',
+            Thra_B22_1='/home/peaks_q_0.05/D2_J2_peaks.narrowPeak',
+            Thra_B22_2='/home/peaks_q_0.05/F2_K2_peaks.narrowPeak',
+            Thra_B22_3='/home/peaks_q_0.05/H2_L2_peaks.narrowPeak',
+            Thra_T3_1='/home/peaks_q_0.05/D3_J3_peaks.narrowPeak')#,
+            Thra_T3_2='/home/peaks_q_0.05/F3_K3_peaks.narrowPeak',
+            Thra_T3_3='/home/peaks_q_0.05/H3_L3_peaks.narrowPeak')
 #-----------merged files 
-files<-list(Thra_0hr='/home/em_b/work_stuff/chipseq/thra_0hr_comb/peaks/Thra_IgG_peaks.narrowPeak',
-            Thra_B22='/home/em_b/work_stuff/chipseq/thra_b22_comb/peaks/Thra_b22_IgG_peaks.narrowPeak',
-            Thra_T3='/home/em_b/work_stuff/chipseq/thra_t3_comb/peaks/Thra_t3_IgG_peaks.narrowPeak')
-#peakAnnoList <- lapply(files, annotatePeak, TxDb=TxDb.Mmusculus.UCSC.mm39.knownGene,)
+files<-list(Thra_0hr='/home/Thra_IgG_peaks.narrowPeak',
+            Thra_B22='/home/Thra_b22_IgG_peaks.narrowPeak',
+            Thra_T3='/home/Thra_t3_IgG_peaks.narrowPeak')
 peakAnnoList <- lapply(files, annotatePeak, TxDb=TxDb.Mmusculus.UCSC.mm39.knownGene,
                        tssRegion=c(-3000, 3000), verbose=FALSE)
 peakAnnoList
@@ -31,7 +30,6 @@ enrichPeakOverlap(queryPeak     = files[[1]],
                   chainFile     = NULL,
                   verbose       = FALSE,
                   mc.cores = 3)
-break 
 #---------------enrichGO biological processes
 test <- compareCluster(geneCluster   = genes,
                            fun           = 'enrichGO',
